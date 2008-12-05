661b5009482e9459f6342ce42d1cfe80d6f1adcf
Vadim Lopatin <vadim.lopatin@coolreader.org>
2008-12-05 13:33:36 +0000

    fix automake files

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-aclocal
-libtoolize --automake --force --copy
-automake -a -c
-autoconf
 
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
+
+touch README
+
+cd crengine && NOCONFIGURE=1 sh autogen.sh && cd ..
+
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+#echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
+
+if [ -z "$NOCONFIGURE" ]; then
+	./configure "$@"
+fi
