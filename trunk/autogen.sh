97e59ea3b8bd88c469d90d58eb6a24acfbab07c4
Vadim Lopatin <vadim.lopatin@coolreader.org>
2008-12-10 20:44:44 +0000

    fix paths

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -11,6 +11,6 @@ echo "Running autoconf..." ; autoconf || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
-if [ -z "$NOCONFIGURE" ]; then
-	./configure "$@"
-fi
+#if [ -z "$NOCONFIGURE" ]; then
+#	./configure "$@"
+#fi
