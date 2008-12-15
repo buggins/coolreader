486913cc20dd20523e94d6d7c72bee5c7516be6f
Vadim Lopatin <vadim.lopatin@coolreader.org>
2008-12-15 11:23:12 +0000

    docview redraw fix

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-#test
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
-touch README
+#touch README
 
+echo "Running autotools for crengine"
 cd crengine && NOCONFIGURE=1 sh autogen.sh && cd ..
+cd dict-plugin && NOCONFIGURE=1 sh autogen.sh && cd ..
 
+echo "Running autotools for cr3ewl"
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 #echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
