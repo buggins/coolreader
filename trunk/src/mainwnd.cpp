e2715f5c0c74b6a10550e97fe46ece1c539616c1
Vadim Lopatin <vadim.lopatin@coolreader.org>
2009-01-29 15:06:16 +0000

    show toc in GoToPage dialog, part 1

--- trunk/src/mainwnd.cpp
+++ trunk/src/mainwnd.cpp
@@ -129,6 +129,121 @@ class CRNumberEditDialog : public CRGUIWindowBase
         }
 };
 
+class CRTOCDialog : public CRNumberEditDialog
+{
+    protected:
+        LVPtrVector<LVTocItem, false> _items;
+        LVTocItem * _toc;
+        LVFontRef _font;
+        //CRWindowSkinRef _inputSkin;
+        virtual void draw()
+        {
+            CRRectSkinRef titleSkin = _skin->getTitleSkin();
+            CRRectSkinRef clientSkin = _skin->getClientSkin();
+            LVRef<LVDrawBuf> drawbuf = _wm->getScreen()->getCanvas();
+            _skin->draw( *drawbuf, _rect );
+            lvRect titleRect = _skin->getTitleRect( _rect );
+            titleSkin->draw( *drawbuf, titleRect );
+            titleSkin->drawText( *drawbuf, titleRect, _title );
+            lvRect clientRect = _skin->getClientRect( _rect );
+            lvRect inputRect = clientRect;
+            inputRect.top = inputRect.bottom - 40;
+            lvRect tocRect = clientRect;
+            tocRect.bottom = inputRect.top;
+            clientSkin->draw( *drawbuf, inputRect );
+            _skin->drawText( *drawbuf, inputRect, lString16("Enter page number to go: ") + _value+L"_" );
+            clientSkin->draw( *drawbuf, tocRect );
+        }
+        void addItem( LVTocItem * item )
+        {
+            _items.add( item );
+            for ( int i=0; i<item->getChildCount(); i++ ) {
+                addItem( item->getChild( i ) );
+            }
+        }
+    public:
+        CRTOCDialog( CRGUIWindowManager * wm, lString16 title, int resultCmd, int pageCount, LVTocItem * toc )
+        : CRNumberEditDialog( wm, title, lString16(), resultCmd, 1, pageCount )
+        , _toc(toc)
+        {
+            addItem( toc );
+            _skin = _wm->getSkin()->getWindowSkin(L"#dialog");
+            CRRectSkinRef clientSkin = _skin->getClientSkin();
+            _font = clientSkin->getFont();
+            _fullscreen = true;
+            _rect = _wm->getScreen()->getRect();
+            lvPoint clientSize( 250, _skin->getFont()->getHeight() + 24 );
+            lvPoint sz = _skin->getWindowSize( clientSize );
+            lvRect rc = _wm->getScreen()->getRect();
+            int x = (rc.width() - sz.x) / 2;
+            int y = (rc.height() - sz.y) / 2;
+            _rect.left = x;
+            _rect.top = y;
+            _rect.right = x + sz.x;
+            _rect.bottom = y + sz.y;
+        }
+        virtual ~CRTOCDialog()
+        {
+        }
+        bool digitEntered( lChar16 c )
+        {
+            lString16 v = _value;
+            v << c;
+            int n = v.atoi();
+            if ( n<=_maxvalue ) {
+                _value = v;
+                setDirty();
+                return true;
+            }
+            return false;
+        }
+
+        /// returns true if command is processed
+        virtual bool onCommand( int command, int params )
+        {
+            switch ( command ) {
+            case MCMD_CANCEL:
+                if ( _value.length()>0 ) {
+                    _value.erase( _value.length()-1, 1 );
+                    setDirty();
+                } else {
+                    _wm->closeWindow( this );
+                }
+                return true;
+            case MCMD_OK:
+                {
+                    int n = _value.atoi();
+                    if ( n>=_minvalue && n<=_maxvalue ) {
+                        _wm->postCommand( _resultCmd, n );
+                        _wm->closeWindow( this );
+                        return true;
+                    }
+                    _wm->closeWindow( this );
+                    return true;
+                }
+            case MCMD_SCROLL_FORWARD:
+                break;
+            case MCMD_SCROLL_BACK:
+                break;
+            case MCMD_SELECT_0:
+            case MCMD_SELECT_1:
+            case MCMD_SELECT_2:
+            case MCMD_SELECT_3:
+            case MCMD_SELECT_4:
+            case MCMD_SELECT_5:
+            case MCMD_SELECT_6:
+            case MCMD_SELECT_7:
+            case MCMD_SELECT_8:
+            case MCMD_SELECT_9:
+                digitEntered( '0' + (command - MCMD_SELECT_0) );
+                break;
+            default:
+                return false;
+            }
+            return true;
+        }
+};
+
 static const char * link_back_active[] = {
 	"30 27 5 1",
 	"0 c #000000",
@@ -1055,10 +1170,19 @@ VIEWER_MENU_4ABOUT=About...
 
 void V3DocViewWin::showGoToPageDialog()
 {
-    CRNumberEditDialog * dlg = new CRNumberEditDialog( _wm, 
-        _wm->translateString("VIEWER_HINT_INPUTSKIPPAGENUM", "Enter page number"),
-        lString16(), 
-        MCMD_GO_PAGE_APPLY, 1, _docview->getPageCount() );
+    LVTocItem * toc = _docview->getToc();
+    CRNumberEditDialog * dlg;
+    if ( toc && toc->getChildCount()>0 ) {
+    
+        dlg = new CRTOCDialog( _wm, 
+            _wm->translateString("VIEWER_HINT_INPUTSKIPPAGENUM", "Enter page number"),
+            MCMD_GO_PAGE_APPLY,  _docview->getPageCount(), toc );
+    } else {
+        dlg = new CRNumberEditDialog( _wm, 
+            _wm->translateString("VIEWER_HINT_INPUTSKIPPAGENUM", "Enter page number"),
+            lString16(), 
+            MCMD_GO_PAGE_APPLY, 1, _docview->getPageCount() );
+    }
     dlg->setAccelerators( _dialogAccelerators );
     _wm->activateWindow( dlg );
 }
