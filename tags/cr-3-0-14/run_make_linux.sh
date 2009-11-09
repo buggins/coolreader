b1cc14b7dbbba153da9b3e5b2e2160f2a99f208b
Vadim Lopatin <lve@lve-desktop.(none)>
2009-11-09 16:52:47 +0300

    renames

--- tags/cr-3-0-14/run_make_linux.sh
+++
@@ -1,9 +0,0 @@
-# build CR3 on linux
-# crengine dir should be placed on the same level as cr3
-# required libs: wxgtk, zlib, libjpeg, libpng, freetype
-ls crengine > /dev/null 2> /dev/null || ln -s ../crengine crengine
-WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -f Makefile.cvs
-ls optimized > /dev/null 2> /dev/null || mkdir optimized
-basedir=`pwd`
-cd $basedir/optimized && CPPFLAGS="-I$basedir/crengine/include -I/usr/include/freetype2 -DLINUX -D_LINUX" CXXFLAGS="-O2 -g0" $basedir/configure \
-&& cd $basedir/optimized && make
