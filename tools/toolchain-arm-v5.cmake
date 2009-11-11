8952bff014c0646e9f13c1f319dcf84f0de1d4e0
Vadim Lopatin <lve@lve-laptop.(none)>
2009-11-11 07:19:51 +0300

    V5 build support

---
+++ tools/toolchain-arm-v5.cmake
@@ -0,0 +1,19 @@
+# CMake toolchain file for building Jinke V3 software using gcc 3.3.4
+
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /opt/arm-9tdmi-linux-gnu/gcc-4.0.0-glibc-2.3.5/bin/arm-9tdmi-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-9tdmi-linux-gnu/gcc-4.0.0-glibc-2.3.5/bin/arm-9tdmi-linux-gnu-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /opt/arm-9tdmi-linux-gnu/gcc-4.0.0-glibc-2.3.5/arm-9tdmi-linux-gnu)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
