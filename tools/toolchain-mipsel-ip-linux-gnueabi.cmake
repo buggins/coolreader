25544b841be59543c1286995269cf245e17affd5
debian_lenny build user <build@lve-desktop.(none)>
2009-11-30 14:14:44 +0000

    fixes for OI debian package

---
+++ tools/toolchain-mipsel-ip-linux-gnueabi.cmake
@@ -0,0 +1,19 @@
+# CMake toolchain file for building MIPS software on OI environment
+
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /usr/bin/mipsel-ip-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/mipsel-ip-linux-gnueabi-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /usr/mipsel-ip-linux-gnueabi)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
