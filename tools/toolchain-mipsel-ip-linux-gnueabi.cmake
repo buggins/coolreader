f802eb225169fbe12cc459e13c5880bc0dea6d0d
Vadim Lopatin <lve@lve-laptop.(none)>
2010-01-23 15:21:35 +0300

    fix mipsel toolchain settings

--- tools/toolchain-mipsel-ip-linux-gnueabi.cmake
+++ tools/toolchain-mipsel-ip-linux-gnueabi.cmake
@@ -6,11 +6,11 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/mipsel-ip-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/mipsel-ip-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER   /usr/bin/mipsel-ip-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/mipsel-ip-linux-gnu-g++)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /usr/mipsel-ip-linux-gnueabi)
+SET(CMAKE_FIND_ROOT_PATH  /usr/mipsel-ip-linux-gnu)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
