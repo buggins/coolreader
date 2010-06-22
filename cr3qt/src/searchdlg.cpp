ff77e5212ba45642935f6426d73b5b702f71878f
Vadim Lopatin <buggins@fromru.com>
2010-06-22 08:40:50 +0400

    implemented Search feature in Qt version

--- cr3qt/src/searchdlg.cpp
+++ cr3qt/src/searchdlg.cpp
@@ -42,10 +42,79 @@ void SearchDialog::changeEvent(QEvent *e)
     }
 }
 
+bool SearchDialog::findText( lString16 pattern, int origin, bool reverse, bool caseInsensitive )
+{
+    if ( pattern.empty() )
+        return false;
+    if ( pattern!=_lastPattern && origin==1 )
+        origin = 0;
+    _lastPattern = pattern;
+    LVArray<ldomWord> words;
+    lvRect rc;
+    _docview->getDocView()->GetPos( rc );
+    int pageHeight = rc.height();
+    int start = -1;
+    int end = -1;
+    if ( reverse ) {
+        // reverse
+        if ( origin == 0 ) {
+            // from end current page to first page
+            end = rc.bottom;
+        } else if ( origin == -1 ) {
+            // from last page to end of current page
+            start = rc.bottom;
+        } else { // origin == 1
+            // from prev page to first page
+            end = rc.top;
+        }
+    } else {
+        // forward
+        if ( origin == 0 ) {
+            // from current page to last page
+            start = rc.top;
+        } else if ( origin == -1 ) {
+            // from first page to current page
+            end = rc.top;
+        } else { // origin == 1
+            // from next page to last
+            start = rc.bottom;
+        }
+    }
+    CRLog::debug("CRViewDialog::findText: Current page: %d .. %d", rc.top, rc.bottom);
+    CRLog::debug("CRViewDialog::findText: searching for text '%s' from %d to %d origin %d", LCSTR(pattern), start, end, origin );
+    if ( _docview->getDocView()->getDocument()->findText( pattern, caseInsensitive, reverse, start, end, words, 200, pageHeight ) ) {
+        CRLog::debug("CRViewDialog::findText: pattern found");
+        _docview->getDocView()->clearSelection();
+        _docview->getDocView()->selectWords( words );
+        ldomMarkedRangeList * ranges = _docview->getDocView()->getMarkedRanges();
+        if ( ranges ) {
+            if ( ranges->length()>0 ) {
+                int pos = ranges->get(0)->start.y;
+                _docview->getDocView()->SetPos(pos);
+            }
+        }
+        return true;
+    }
+    CRLog::debug("CRViewDialog::findText: pattern not found");
+    return false;
+}
+
 void SearchDialog::on_btnFindNext_clicked()
 {
-    QMessageBox * mb = new QMessageBox( QMessageBox::Information, tr("Not implemented"), tr("Search is not implemented yet"), QMessageBox::Close, this );
-    mb->exec();
+    bool found = false;
+    QString pattern = ui->edPattern->text();
+    lString16 p16 = qt2cr(pattern);
+    bool reverse = ui->rbBackward->isChecked();
+    bool caseInsensitive = ui->cbCaseSensitive->checkState()!=Qt::Checked;
+    found = findText(p16, 1, reverse , caseInsensitive);
+    if ( !found )
+        found = findText(p16, -1, reverse, caseInsensitive);
+    if ( !found ) {
+        QMessageBox * mb = new QMessageBox( QMessageBox::Information, tr("Not found"), tr("Search pattern is not found in document"), QMessageBox::Close, this );
+        mb->exec();
+    } else {
+        _docview->update();
+    }
 }
 
 void SearchDialog::on_btnClose_clicked()
--- cr3qt/src/searchdlg.h
+++ cr3qt/src/searchdlg.h
@@ -1,6 +1,7 @@
 #ifndef SEARCHDLG_H
 #define SEARCHDLG_H
 
+#include <lvstring.h>
 #include <QDialog>
 
 namespace Ui {
@@ -17,12 +18,14 @@ public:
 
     static bool showDlg( QWidget * parent, CR3View * docView );
 
+    bool findText( lString16 pattern, int origin, bool reverse, bool caseInsensitive );
 protected:
     void changeEvent(QEvent *e);
 
 private:
     Ui::SearchDialog *ui;
     CR3View * _docview;
+    lString16 _lastPattern;
 private slots:
     void on_btnFindNext_clicked();
     void on_btnClose_clicked();
