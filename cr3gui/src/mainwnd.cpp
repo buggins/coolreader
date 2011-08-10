621f3bda91a84e66783d598e8ec5a365a6312798
Konstantin Potapov <pkbo@users.sourceforge.net>
2011-08-10 08:02:34 +0700

    uncommented font size setting for book properties window

--- cr3gui/src/mainwnd.cpp
+++ cr3gui/src/mainwnd.cpp
@@ -1063,8 +1063,8 @@ void V3DocViewWin::showAboutDialog()
     txt = CRViewDialog::makeFb2Xml(txt);
     CRViewDialog * dlg = new CRViewDialog( _wm, title, txt, lvRect(), true, true );
     dlg->getDocView()->setVisiblePageCount(1);
-    //int fs = _props->getIntDef( PROP_FILE_PROPS_FONT_SIZE, 26 );
-    //dlg->getDocView()->setFontSize(fs);
+    int fs = _props->getIntDef( PROP_FILE_PROPS_FONT_SIZE, 26 );
+    dlg->getDocView()->setFontSize(fs);
     _wm->activateWindow( dlg );
 }
 
