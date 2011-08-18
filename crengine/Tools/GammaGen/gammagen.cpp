11f8b8246ebe41a6bbf9d6936e74054a60b4c136
Vadim Lopatin <buggins@fromru.com>
2011-08-18 15:52:47 +0400

    Font Gamma: increase available range up to 0.3 .. 1.9

--- crengine/Tools/GammaGen/gammagen.cpp
+++ crengine/Tools/GammaGen/gammagen.cpp
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define GAMMA_LEVELS 23
+#define GAMMA_LEVELS 31
 
-static const double gamma_levels[GAMMA_LEVELS] = { 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.98, 1, 1.02, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5 };
+static const double gamma_levels[GAMMA_LEVELS] = { 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.98, 1, 1.02, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.6, 1.7, 1.8, 1.9 };
 
 void genTable( FILE * out, double gamma, int index ) {
     fprintf(out, "static const unsigned char gamma_table_%d[256] = { // gamma=%f\n    ", index, gamma);
