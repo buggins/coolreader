ac54991fd8f16e71ce9f0fc55da2bfd4db2d2a9f
Vadim Lopatin <buggins@fromru.com>
2011-12-02 09:05:13 +0400

    Android: remove x86 JNI from package

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_OPTIM := release
 #APP_OPTIM := debug
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a
 #armeabi-v7a
