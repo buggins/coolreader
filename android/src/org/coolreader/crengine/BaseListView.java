744b227ece2c46efd5dc4dd530b41de8291a489c
Vadim Lopatin <buggins@fromru.com>
2011-12-29 14:14:20 +0400

    Android: fix lists scrolling using navigation keys

--- android/src/org/coolreader/crengine/BaseListView.java
+++ android/src/org/coolreader/crengine/BaseListView.java
@@ -18,25 +18,30 @@ public class BaseListView  extends ListView {
 			dir = 1;
 		if (keyCode == ReaderView.SONY_DPAD_UP_SCANCODE || keyCode == ReaderView.NOOK_KEY_PREV_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT || keyCode == ReaderView.NOOK_KEY_SHIFT_UP)
 			dir = -1;
+//		boolean tm = isInTouchMode();
 		if (dir != 0) {
-			int top = 0; //getTop();
-			if (dir > 0) {
-				// scroll down
-				int bottompos = pointToPosition(this.getWidth() / 2, getHeight() * 4/5);
-				if (bottompos != INVALID_POSITION) {
-					this.setSelectionFromTop(bottompos, top + 0);
-				}
-			} else {
-				// scroll up
-				int toppos = getFirstVisiblePosition();//  pointToPosition(this.getWidth() / 2, 20);
-				if (toppos != INVALID_POSITION) {
-					this.setSelectionFromTop(toppos, top + getHeight() * 4/5);
-					toppos = getFirstVisiblePosition(); //pointToPosition(this.getWidth() / 2, 20);
-					if (toppos != INVALID_POSITION) {
-						this.setSelectionFromTop(toppos, top + 0);
-					}
-				}
-			}
+			long ts = android.os.SystemClock.uptimeMillis();
+			KeyEvent ev = new KeyEvent(ts, ts, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_SPACE, 0, dir > 0 ? 0 : KeyEvent.META_SHIFT_ON);
+			super.onKeyDown(KeyEvent.KEYCODE_SPACE, ev);
+//			int top = 0; //getTop();
+//			if (dir > 0) {
+//				// scroll down
+//				int bottompos = pointToPosition(this.getWidth() / 2, getHeight() * 4/5);
+//				if (bottompos != INVALID_POSITION) {
+//					this.setSelectionFromTop(bottompos, top + 0);
+//				}
+//			} else {
+//				// scroll up
+//				int toppos = getFirstVisiblePosition();//  pointToPosition(this.getWidth() / 2, 20);
+//				if (toppos != INVALID_POSITION) {
+//					this.setSelectionFromTop(toppos, top + getHeight() * 4/5);
+//					toppos = pointToPosition(this.getWidth() / 2, 20);
+//					toppos = getFirstVisiblePosition(); //pointToPosition(this.getWidth() / 2, 20);
+//					if (toppos != INVALID_POSITION) {
+//						this.setSelectionFromTop(toppos, top + 0);
+//					}
+//				}
+//			}
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
