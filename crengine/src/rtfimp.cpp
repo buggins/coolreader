b134a435b977fb0dcdc22548258392dcd4e91bff
Vadim Lopatin <buggins@fromru.com>
2012-01-10 12:24:45 +0400

    RTF support fix: missing space after unicode character

--- crengine/src/rtfimp.cpp
+++ crengine/src/rtfimp.cpp
@@ -536,8 +536,8 @@ bool LVRtfParser::Parse()
                 // \uN -- unicode character
                 if ( cwi==1 && cwname[0]=='u' ) {
                     AddChar( (lChar16) (param & 0xFFFF) );
-                    if ( m_stack.getInt( pi_skip_ch_count )==0 )
-                        m_stack.set( pi_skip_ch_count, 1 );
+//                    if ( m_stack.getInt( pi_skip_ch_count )==0 )
+//                        m_stack.set( pi_skip_ch_count, 1 );
                 } else {
                     // usual control word
                     OnControlWord( cwname, param, asteriskFlag );
