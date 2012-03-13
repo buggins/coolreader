526745102a8847838b0b0e73cb27b8d9aa436d0f
Vadim Lopatin <buggins@fromru.com>
2012-03-13 21:53:03 +0400

    Android: JNI library for MIPS, X86, ARM

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,8 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_OPTIM := release
 #APP_OPTIM := debug
-APP_ABI := armeabi armeabi-v7a 
+APP_ABI := armeabi mips x86
+#armeabi-v7a mips
 #mips mips-r2
 #x86
 #armeabi-v7a
