7711cd5c3a569eba8819c68b0a65f7d4520d30a6
Vadim Lopatin <buggins@fromru.com>
2012-03-14 09:57:19 +0400

    CRGUI: fix .otf extension support for font files, patch by Olexandr Nesterenko olexn at ukr.net

--- crengine/src/lvfntman.cpp
+++ crengine/src/lvfntman.cpp
@@ -1942,7 +1942,7 @@ public:
                 lString8 fn( (const char *)s );
                 lString16 fn16( fn.c_str() );
                 fn16.lowercase();
-                if ( !fn16.endsWith(L".ttf") && !fn16.endsWith(L".odf") && !fn16.endsWith(L".pfb") && !fn16.endsWith(L".pfa")  ) {
+                if ( !fn16.endsWith(L".ttf") && !fn16.endsWith(L".odf") && !fn16.endsWith(L".otf") && !fn16.endsWith(L".pfb") && !fn16.endsWith(L".pfa")  ) {
                     continue;
                 }
                 int weight = FC_WEIGHT_MEDIUM;
