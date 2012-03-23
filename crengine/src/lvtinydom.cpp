79bd99ec55a7368dbea5dda0f5f2217a23c91840
Vadim Lopatin <buggins@fromru.com>
2012-03-23 09:24:47 +0400

    increment cache file version

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -13,7 +13,7 @@
 
 /// change in case of incompatible changes in swap/cache file format to avoid using incompatible swap file
 // increment to force complete reload/reparsing of old file
-#define CACHE_FILE_FORMAT_VERSION "3.04.34"
+#define CACHE_FILE_FORMAT_VERSION "3.04.35"
 /// increment following value to force re-formatting of old book after load
 #define FORMATTING_VERSION_ID 0x0003
 
