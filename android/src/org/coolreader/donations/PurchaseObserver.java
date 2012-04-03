bd0b1396e8ab8476bc3c453ae4327cfee447b496
Vadim Lopatin <buggins@fromru.com>
2012-04-03 23:01:03 +0400

    Android: fix Android 1.5 incompatibility in donation purachase code

--- android/src/org/coolreader/donations/PurchaseObserver.java
+++ android/src/org/coolreader/donations/PurchaseObserver.java
@@ -30,10 +30,20 @@ public abstract class PurchaseObserver {
     private final Activity mActivity;
     private final Handler mHandler;
     private Method mStartIntentSender;
+    private Method mGetIntentSender;
     private Object[] mStartIntentSenderArgs = new Object[5];
+    private static final Class mIntentSenderClass = findClass("android.content.IntentSender");
     private static final Class[] START_INTENT_SENDER_SIG = new Class[] {
-        IntentSender.class, Intent.class, int.class, int.class, int.class
+    	mIntentSenderClass, Intent.class, int.class, int.class, int.class
     };
+    
+    private static Class findClass(String name) {
+    	try {
+			return Class.forName(name);
+		} catch (ClassNotFoundException e) {
+			return null; // no such class
+		}
+    }
 
     public PurchaseObserver(Activity activity, Handler handler) {
         mActivity = activity;
@@ -104,22 +114,25 @@ public abstract class PurchaseObserver {
         try {
             mStartIntentSender = mActivity.getClass().getMethod("startIntentSender",
                     START_INTENT_SENDER_SIG);
+            mGetIntentSender = PendingIntent.class.getMethod("getIntentSender");
         } catch (SecurityException e) {
             mStartIntentSender = null;
+            mGetIntentSender = null;
         } catch (NoSuchMethodException e) {
             mStartIntentSender = null;
+            mGetIntentSender = null;
         }
     }
 
     void startBuyPageActivity(PendingIntent pendingIntent, Intent intent) {
-        if (mStartIntentSender != null) {
+        if (mStartIntentSender != null && mGetIntentSender != null) {
             // This is on Android 2.0 and beyond.  The in-app buy page activity
             // must be on the activity stack of the application.
             try {
                 // This implements the method call:
                 // mActivity.startIntentSender(pendingIntent.getIntentSender(),
                 //     intent, 0, 0, 0);
-                mStartIntentSenderArgs[0] = pendingIntent.getIntentSender();
+                mStartIntentSenderArgs[0] = mGetIntentSender.invoke(pendingIntent);
                 mStartIntentSenderArgs[1] = intent;
                 mStartIntentSenderArgs[2] = Integer.valueOf(0);
                 mStartIntentSenderArgs[3] = Integer.valueOf(0);
