6f8ed8dd97c88faf2487816e81c9eed9fdd73bf5
Vadim Lopatin <buggins@fromru.com>
2012-08-10 01:30:06 +0400

    Android: litres plugin

--- android/src/org/coolreader/plugins/OnlineStoreBooks.java
+++ android/src/org/coolreader/plugins/OnlineStoreBooks.java
@@ -1,6 +1,10 @@
 package org.coolreader.plugins;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.coolreader.crengine.Utils;
 
 
 public class OnlineStoreBooks extends AsyncResponse {
@@ -16,5 +20,29 @@ public class OnlineStoreBooks extends AsyncResponse {
 	}
 	public OnlineStoreBook get(int index) {
 		return list.get(index);
+	}
+	
+	public void sortBySeriesAndTitle() {
+		Collections.sort(list, new Comparator<OnlineStoreBook>() {
+			@Override
+			public int compare(OnlineStoreBook lhs, OnlineStoreBook rhs) {
+				if (lhs.sequenceName != null) {
+					if (rhs.sequenceName == null)
+						return -1;
+					int res = lhs.sequenceName.compareToIgnoreCase(rhs.sequenceName);
+					if (res != 0)
+						return res;
+					if (lhs.sequenceNumber < rhs.sequenceNumber)
+						return -1;
+					else if (lhs.sequenceNumber > rhs.sequenceNumber)
+						return 1;
+					else
+						return Utils.cmp(lhs.bookTitle, rhs.bookTitle);
+				}
+				if (rhs.sequenceName != null)
+					return 1;
+				return Utils.cmp(lhs.bookTitle, rhs.bookTitle);
+			}
+		});
 	}
 }
