13dd144c8771e094437f6991660080c00d0d5c80
Vadim Lopatin <buggins@fromru.com>
2014-06-17 11:26:38 +0400

    merge patch #25 Build error with latest Android SDK - by Colin McQuillan

--- android/jni/cr3engine.cpp
+++ android/jni/cr3engine.cpp
@@ -511,10 +511,12 @@ protected:
 
 void cr3androidFatalErrorHandler(int errorCode, const char * errorText )
 {
-	static char str[1001];
-	snprintf(str, 1000, "CoolReader Fatal Error #%d: %s", errorCode, errorText);
 	LOGE("CoolReader Fatal Error #%d: %s", errorCode, errorText);
-	LOGASSERTFAILED(errorText, "CoolReader Fatal Error #%d: %s", errorCode, errorText);
+	LOGASSERTFAILED("CoolReader Fatal Error", "CoolReader Fatal Error #%d: %s", errorCode, errorText);
+	//static char str[1001];
+	//snprintf(str, 1000, "CoolReader Fatal Error #%d: %s", errorCode, errorText);
+	//LOGE("CoolReader Fatal Error #%d: %s", errorCode, errorText);
+	//LOGASSERTFAILED(errorText, "CoolReader Fatal Error #%d: %s", errorCode, errorText);
 }
 
 /// set fatal error handler
