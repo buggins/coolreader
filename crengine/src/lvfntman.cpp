c198883b927020510f8fb197951fe64328b8e0b2
Vadim Lopatin <buggins@fromru.com>
2014-09-09 10:58:36 +0400

    support FreeType2 - using USE_FREETYPE2 definition

--- crengine/src/lvfntman.cpp
+++ crengine/src/lvfntman.cpp
@@ -43,11 +43,20 @@
 //#include <ft2build.h>
 
 #ifdef ANDROID
+#ifdef USE_FREETYPE2
+#include "freetype2/config/ftheader.h"
+#include "freetype2/freetype.h"
+#else
 #include "freetype/config/ftheader.h"
 #include "freetype/freetype.h"
+#endif
 #else
 
+#ifdef USE_FREETYPE2
+#include <freetype2/config/ftheader.h>
+#else
 #include <freetype/config/ftheader.h>
+#endif
 //#include <ft2build.h>
 #include FT_FREETYPE_H
 //#include <freetype/freetype.h>
