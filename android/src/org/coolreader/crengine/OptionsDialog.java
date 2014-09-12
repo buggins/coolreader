3ed8ca11c66f48b417a5357e1cbdb78c30d6636b
Vadim Lopatin <buggins@fromru.com>
2014-09-12 11:47:03 +0400

    more page margin values

--- android/src/org/coolreader/crengine/OptionsDialog.java
+++ android/src/org/coolreader/crengine/OptionsDialog.java
@@ -92,7 +92,7 @@ public class OptionsDialog extends BaseDialog implements TabContentFactory, Opti
 			50, 60, 70, 80, 90, 100
 		};
 	int[] mMargins = new int[] {
-			0, 1, 2, 3, 4, 5, 8, 10, 12, 15, 20, 25, 30, 40, 50, 60, 80, 100, 200, 300
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 25, 30, 40, 50, 60, 80, 100, 200, 300
 		};
 	double[] mGammas = new double[] {
 			0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.5, 1.9
