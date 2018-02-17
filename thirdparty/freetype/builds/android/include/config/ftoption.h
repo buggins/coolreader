b52ddb49a5472c133aead413621384bb0d27211f
Aleksey Chernov <valexlin@gmail.com>
2018-02-17 23:05:48 +0400

    Android: bundled freetype configuration files moved into separate folder, also enabled in this configuration bundled libpng.

--- thirdparty/freetype/builds/android/include/config/ftoption.h
+++ thirdparty/freetype/builds/android/include/config/ftoption.h
@@ -263,7 +263,7 @@ FT_BEGIN_HEADER
   /*   options set by those programs have precendence, overwriting the     */
   /*   value here with the configured one.                                 */
   /*                                                                       */
-/* #undef FT_CONFIG_OPTION_USE_PNG */
+#define FT_CONFIG_OPTION_USE_PNG
 
 
   /*************************************************************************/
