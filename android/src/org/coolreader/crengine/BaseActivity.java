45d5e73ede956b110b9a8e37b0f3ed57eb174454
S-trace <S-trace@list.ru>
2018-04-23 14:12:27 +0300

    [MOTIONWATCHDOG] Add basic motion watchdog (timeout may be set in options)

--- android/src/org/coolreader/crengine/BaseActivity.java
+++ android/src/org/coolreader/crengine/BaseActivity.java
@@ -1546,6 +1546,7 @@ public class BaseActivity extends Activity implements Settings {
 	        props.applyDefault(ReaderView.PROP_APP_SCREEN_BACKLIGHT_LOCK, "3");
 	        if ("1".equals(props.getProperty(ReaderView.PROP_APP_SCREEN_BACKLIGHT_LOCK)))
 	            props.setProperty(ReaderView.PROP_APP_SCREEN_BACKLIGHT_LOCK, "3");
+	        props.applyDefault(ReaderView.PROP_APP_MOTION_TIMEOUT, "0");
 	        props.applyDefault(ReaderView.PROP_APP_BOOK_PROPERTY_SCAN_ENABLED, "1");
 	        props.applyDefault(ReaderView.PROP_APP_KEY_BACKLIGHT_OFF, DeviceInfo.SAMSUNG_BUTTONS_HIGHLIGHT_PATCH ? "0" : "1");
 	        props.applyDefault(ReaderView.PROP_LANDSCAPE_PAGES, DeviceInfo.ONE_COLUMN_IN_LANDSCAPE ? "0" : "1");
