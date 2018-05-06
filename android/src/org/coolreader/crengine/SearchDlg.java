70f2ebb3e93ead4f0e5128653d6ed38e84a689db
Yuri Plotnikov <plotnikovya@gmail.com>
2018-05-06 13:10:16 +0300

    search history

--- android/src/org/coolreader/crengine/SearchDlg.java
+++ android/src/org/coolreader/crengine/SearchDlg.java
@@ -1,14 +1,25 @@
 package org.coolreader.crengine;
 
+import org.coolreader.Dictionaries;
 import org.coolreader.R;
+import org.coolreader.db.CRDBService;
 
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class SearchDlg  extends BaseDialog {
+import java.util.ArrayList;
+
+public class SearchDlg extends BaseDialog {
 	BaseActivity mCoolReader;
 	ReaderView mReaderView;
 	private LayoutInflater mInflater;
@@ -16,7 +27,11 @@ public class SearchDlg  extends BaseDialog {
 	EditText mEditView;
 	CheckBox mCaseSensitive;
 	CheckBox mReverse;
-	
+	BookInfo mBookInfo;
+	ArrayList<String> mSearches;
+	private SearchList mList;
+
+
 	@Override
 	protected void onPositiveButtonClick()
 	{
@@ -25,6 +40,8 @@ public class SearchDlg  extends BaseDialog {
     	if ( pattern==null || pattern.length()==0 ) 
     		mCoolReader.showToast("No pattern specified");
     	else
+			activity.getDB().saveSearchHistory(mBookInfo,
+					mEditView.getText().toString());
     		mReaderView.findText( mEditView.getText().toString(), mReverse.isChecked(), !mCaseSensitive.isChecked() );
         cancel();
 	}
@@ -36,6 +53,102 @@ public class SearchDlg  extends BaseDialog {
         cancel();
 	}
 
+	public final static int ITEM_POSITION=0;
+
+	class SearchListAdapter extends BaseAdapter {
+		public boolean areAllItemsEnabled() {
+			return true;
+		}
+
+		public boolean isEnabled(int arg0) {
+			return true;
+		}
+
+		public int getCount() {
+			return mSearches.size();
+			//return Dictionaries.getDictListExt(mCoolReader,true).size();
+		}
+
+		public Object getItem(int position) {
+			if ( position<0 || position>=mSearches.size() )
+				return null;
+			return mSearches.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public int getItemViewType(int position) {
+			return ITEM_POSITION;
+		}
+
+		public int getViewTypeCount() {
+			return 4;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view;
+			int res = R.layout.dict_item;
+			view = mInflater.inflate(res, null);
+			TextView labelView = (TextView)view.findViewById(R.id.dict_item_shortcut);
+			TextView titleTextView = (TextView)view.findViewById(R.id.dict_item_title);
+			String s = (String)getItem(position);
+			if ( labelView!=null ) {
+				labelView.setText(String.valueOf(position+1));
+			}
+			if ( s!=null ) {
+				if ( titleTextView!=null )
+					titleTextView.setText(s);
+			} else {
+				if ( titleTextView!=null )
+					titleTextView.setText("");
+			}
+			return view;
+		}
+
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		public boolean isEmpty() {
+			return Dictionaries.getDictList().length==0;
+		}
+
+		private ArrayList<DataSetObserver> observers = new ArrayList<DataSetObserver>();
+
+		public void registerDataSetObserver(DataSetObserver observer) {
+			observers.add(observer);
+		}
+
+		public void unregisterDataSetObserver(DataSetObserver observer) {
+			observers.remove(observer);
+		}
+	}
+
+	class SearchList extends BaseListView {
+
+		public SearchList(Context context, boolean shortcutMode ) {
+			super(context, true);
+			setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+			setLongClickable(true);
+			setAdapter(new SearchDlg.SearchListAdapter());
+			setOnItemLongClickListener(new OnItemLongClickListener() {
+				@Override
+				public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+											   int position, long arg3) {
+					openContextMenu(SearchDlg.SearchList.this);
+					return true;
+				}
+			});
+		}
+
+		@Override
+		public boolean performItemClick(View view, int position, long id) {
+			mEditView.setText(mSearches.get(position));
+			return true;
+		}
+	}
 	
 	public SearchDlg(BaseActivity coolReader, ReaderView readerView, String initialText)
 	{
@@ -43,6 +156,7 @@ public class SearchDlg  extends BaseDialog {
         setCancelable(true);
 		this.mCoolReader = coolReader;
 		this.mReaderView = readerView;
+		this.mBookInfo = mReaderView.getBookInfo();
 		setPositiveButtonImage(R.drawable.cr3_button_find, R.string.action_search);
         mInflater = LayoutInflater.from(getContext());
         mDialogView = mInflater.inflate(R.layout.search_dialog, null);
@@ -51,6 +165,17 @@ public class SearchDlg  extends BaseDialog {
     		mEditView.setText(initialText);
     	mCaseSensitive = (CheckBox)mDialogView.findViewById(R.id.search_case_sensitive);
     	mReverse = (CheckBox)mDialogView.findViewById(R.id.search_reverse);
+		activity.getDB().loadSearchHistory(this.mBookInfo, new CRDBService.SearchHistoryLoadingCallback() {
+			@Override
+			public void onSearchHistoryLoaded(ArrayList<String> searches) {
+				mSearches = searches;
+				ViewGroup body = (ViewGroup)mDialogView.findViewById(R.id.history_list);
+				mList = new SearchDlg.SearchList(activity, false);
+				body.addView(mList);
+			}
+		});
+		//setView(mDialogView);
+		//setFlingHandlers(mList, null, null);
 		// setup buttons
 	}
 
