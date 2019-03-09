buggins/coolreader

commit fe9b4ff91c98dd88ccc5d27396597fa49aeb9d85

Proper handling of a document loading error.

android/jni/docview.cpp
@@ -867,11 +867,27 @@ static DocViewNative * getNative(JNIEnv * env, jobject _this)
 	return res;
 }
 
+void DocViewNative::createDefaultDocument( lString16 title, lString16 message )
+{
+	_docview->createDefaultDocument(title, message);
+}
+
 bool DocViewNative::loadDocument( lString16 filename )
 {
 	CRLog::info("Loading document %s", LCSTR(filename));
 	bool res = _docview->LoadDocument(filename.c_str());
-	CRLog::info("Document %s is loaded %s", LCSTR(filename), (res?"successfully":"with error"));
+	if (res)
+		CRLog::info("Document %s is loaded successfully", LCSTR(filename));
+	else {
+		CRLog::info("Document %s not is loaded due to error", LCSTR(filename));
+		if (_docview->getDocument() == NULL) {
+			// _docview->LoadDocument() can return false with _docview->m_doc == NULL when:
+			// 1. I/O error - failed to open file
+			// 2. open archive without supported files
+			CRLog::error("Document is NULL, inserting stub.");
+			_docview->createDefaultDocument(lString16::empty_str, Utf8ToUnicode("Error while opening file!"));
+		}
+	}
     return res;
 }
 
@@ -882,8 +898,8 @@ bool DocViewNative::openRecentBook()
 	if ( _docview->isDocumentOpened() ) {
 		CRLog::debug("DocViewNative::openRecentBook() : saving previous document state");
 		_docview->swapToCache();
-        _docview->getDocument()->updateMap();
-	    _docview->savePosition();
+		_docview->getDocument()->updateMap();
+		_docview->savePosition();
 		closeBook();
 	    index = 1;
 	}
@@ -895,7 +911,12 @@ bool DocViewNative::openRecentBook()
         CRLog::info("DocViewNative::openRecentBook() : checking file %s", LCSTR(fn));
         // TODO: check error
         if ( LVFileExists(fn) ) {
-            return loadDocument( fn );
+            bool res = loadDocument( fn );
+            if (!res && _docview->getDocument() == NULL) {
+                CRLog::error("Document is NULL, inserting stub.");
+                _docview->createDefaultDocument(lString16::empty_str, Utf8ToUnicode("Error while opening file!"));
+            }
+            return res;
         } else {
         	CRLog::error("file %s doesn't exist", LCSTR(fn));
         	return false;
@@ -1147,7 +1168,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_checkImageIntern
     DocViewNative * p = getNative(_env, view);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
     int dx, dy;
     bool needRotate = false;
@@ -1177,7 +1198,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_checkBookmarkInt
     DocViewNative * p = getNative(_env, view);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
     CRObjectAccessor acc(_env, bmk);
     //CRLog::trace("checkBookmarkInternal(%d, %d)", x, y);
@@ -1205,7 +1226,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_drawImageInterna
     DocViewNative * p = getNative(_env, view);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
     CRObjectAccessor acc(_env, imageInfo);
     int dx = CRIntField(acc,"scaledWidth").get();
@@ -1266,9 +1287,28 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_closeImageIntern
     DocViewNative * p = getNative(env, view);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	return p->closeImage() ? JNI_TRUE : JNI_FALSE;
+}
+
+/*
+ * Class:     org_coolreader_crengine_DocView
+ * Method:    createDefaultDocumentInternal
+ * Signature: (Ljava/lang/String;Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_createDefaultDocumentInternal
+		(JNIEnv * _env, jobject _this, jstring title, jstring message)
+{
+	CRJNIEnv env(_env);
+	DocViewNative * p = getNative(_env, _this);
+	if (!p) {
+		CRLog::error("Cannot get native view");
+		return;
+	}
+	lString16 title_str = env.fromJavaString(title);
+	lString16 message_str = env.fromJavaString(message);
+	p->createDefaultDocument(title_str, message_str);
 }
 
 /*
@@ -1283,7 +1323,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_loadDocumentInte
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	DocViewCallback callback( _env, p->_docview, _this );
 	lString16 str = env.fromJavaString(s);
@@ -1325,7 +1365,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_applySettingsInt
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	DocViewCallback callback( _env, p->_docview, _this );
 	CRPropRef props = env.fromJavaProperties(_props);
@@ -1423,12 +1463,14 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_doCommandInterna
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	DocViewCallback callback( _env, p->_docview, _this );	
     if ( cmd>=READERVIEW_DCMD_START && cmd<=READERVIEW_DCMD_END) {
     	return p->doCommand(cmd, param)?JNI_TRUE:JNI_FALSE;
     }
+    if (!p->_docview->isDocumentOpened())
+        return JNI_FALSE;
     //CRLog::trace("doCommandInternal(%d, %d) -- passing to LVDocView", cmd, param);
     return p->_docview->doCommand((LVDocCmd)cmd, param) ? JNI_TRUE : JNI_FALSE;
 }
@@ -1445,7 +1487,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_isRenderedIntern
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	if (!p->_docview->isDocumentOpened())
 		return JNI_FALSE;
@@ -1469,7 +1511,7 @@ JNIEXPORT jobject JNICALL Java_org_coolreader_crengine_DocView_getCurrentPageBoo
 	CRLog::trace("getCurrentPageBookmarkInternal: calling getBookmark()");
 	ldomXPointer ptr = p->_docview->getBookmark();
 	if ( ptr.isNull() )
-		return JNI_FALSE;
+		return NULL;
 	CRBookmark bm(ptr);
 	lString16 comment;
     lString16 titleText;
@@ -1553,7 +1595,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_goToPositionInte
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
 	if ( !p->_docview->isDocumentOpened() )
 		return JNI_FALSE;
@@ -1675,7 +1717,7 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_findTextInternal
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
     if ( !p->_docview->isDocumentOpened() )
         return JNI_FALSE;
@@ -1842,8 +1884,12 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_moveSelectionInt
     DocViewNative * p = getNative(_env, _this);
     if (!p) {
     	CRLog::error("Cannot get native view");
-    	return false;
+    	return JNI_FALSE;
     }
+	if (!p->_docview->isDocumentOpened()) {
+		CRLog::debug("moveSelectionInternal: document is not opened");
+		return JNI_FALSE;
+	}
     CRObjectAccessor sel(_env, _sel);
     CRStringField sel_startPos(sel, "startPos");
     CRStringField sel_endPos(sel, "endPos");
@@ -1939,7 +1985,7 @@ bool DocViewNative::checkImage(int x, int y, int bufWidth, int bufHeight, int &d
 	dy = _currentImage->GetHeight();
 	if (dx < 8 && dy < 8) {
 		_currentImage.Clear();
-		return JNI_FALSE;
+		return false;
 	}
 	needRotate = false;
 	if (bufWidth <= bufHeight) {

android/jni/docview.h
@@ -28,6 +28,7 @@ public:
 	bool closeBook();
 	bool loadHistory( lString16 filename );
 	bool saveHistory( lString16 filename );
+	void createDefaultDocument( lString16 title, lString16 message );
 	bool loadDocument( lString16 filename );
 	int doCommand( int cmd, int param );
     bool findText( lString16 pattern, int origin, bool reverse, bool caseInsensitive );

android/jni/org_coolreader_crengine_DocView.h
@@ -39,6 +39,14 @@ JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_destroyInternal
 
 /*
  * Class:     org_coolreader_crengine_DocView
+ * Method:    createDefaultDocumentInternal
+ * Signature: (Ljava/lang/String;Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_createDefaultDocumentInternal
+  (JNIEnv *, jobject, jstring, jstring);
+
+/*
+ * Class:     org_coolreader_crengine_DocView
  * Method:    loadDocumentInternal
  * Signature: (Ljava/lang/String;)Z
  */

android/res/values-ru/strings.xml
@@ -589,4 +589,5 @@
     <string name="pages_per_full_swipe_20">"20 страниц (отслеживать)"</string>
     <string name="options_app_ui_theme_hicontrast1">Контрастная белая</string>
     <string name="options_app_ui_theme_hicontrast2">Контрастная черная</string>
+    <string name="error_while_opening">Ошибка открытия документа \"%s\"</string>
 </resources>

android/res/values/strings.xml
@@ -615,4 +615,5 @@
     <string name="pages_per_full_swipe_18">"18 pages (track)"</string>
     <string name="pages_per_full_swipe_19">"19 pages (track)"</string>
     <string name="pages_per_full_swipe_20">"20 pages (track)"</string>
+    <string name="error_while_opening">Error while opening document \"%s\"</string>
 </resources>

android/src/org/coolreader/crengine/DocView.java
@@ -136,6 +136,19 @@ public class DocView {
 	}
 
 	/**
+	 * create empty document with specified message (e.g. to show errors)
+	 * @param title
+	 * @param message
+	 * @return
+	 */
+	public void createDefaultDocument(String title, String message)
+	{
+		synchronized(mutex) {
+			createDefaultDocumentInternal(title, message);
+		}
+	}
+
+	/**
 	 * Load document from file.
 	 * @param fileName
 	 * @return
@@ -393,6 +406,8 @@ public class DocView {
 	private native void createInternal();
 
 	private native void destroyInternal();
+
+	private native void createDefaultDocumentInternal(String title, String message);
 
 	private native boolean loadDocumentInternal(String fileName);
 

android/src/org/coolreader/crengine/ReaderView.java
@@ -1012,6 +1012,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 		}
 
 		private void updatePageFlipTracking(final int x, final int y) {
+			if (!mOpened)
+				return;
 			final int swipeDistance = mIsPageMode ? x - start_x : y - start_y;
 			final int distanceForFlip = surface.getWidth() / mGesturePageFlipsPerFullSwipe;
 			int pagesToFlip = swipeDistance / distanceForFlip;
@@ -2277,7 +2279,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			});
 			break;
 		case DCMD_BOOK_INFO:
-			showBookInfo();
+			if (isBookLoaded())
+				showBookInfo();
 			break;
 		case DCMD_USER_MANUAL:
 			showManual();
@@ -2303,7 +2306,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			}
 			break;
 		case DCMD_TOGGLE_DOCUMENT_STYLES:
-			toggleDocumentStyles();
+			if (isBookLoaded())
+				toggleDocumentStyles();
 			break;
 		case DCMD_SHOW_HOME_SCREEN:
 			mActivity.showHomeScreen();
@@ -2318,10 +2322,12 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			toggleTitlebar();
 			break;
 		case DCMD_SHOW_POSITION_INFO_POPUP:
-			showReadingPositionPopup();
+			if (isBookLoaded())
+				showReadingPositionPopup();
 			break;
 		case DCMD_TOGGLE_SELECTION_MODE:
-			toggleSelectionMode();
+			if (isBookLoaded())
+				toggleSelectionMode();
 			break;
 		case DCMD_TOGGLE_TOUCH_SCREEN_LOCK:
 			isTouchScreenEnabled = !isTouchScreenEnabled;
@@ -2349,45 +2355,56 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			switchFontFace(-1);
             break;
 		case DCMD_MOVE_BY_CHAPTER:
-			doEngineCommand(cmd, param, onFinishHandler);
+			if (isBookLoaded())
+				doEngineCommand(cmd, param, onFinishHandler);
             drawPage();
 			break;
 		case DCMD_PAGEDOWN:
-			if ( param==1 && !DeviceInfo.EINK_SCREEN)
-				animatePageFlip(1, onFinishHandler);
-			else
-				doEngineCommand(cmd, param, onFinishHandler);
+			if (isBookLoaded()) {
+				if (param == 1 && !DeviceInfo.EINK_SCREEN)
+					animatePageFlip(1, onFinishHandler);
+				else
+					doEngineCommand(cmd, param, onFinishHandler);
+			}
 			break;
 		case DCMD_PAGEUP:
-			if ( param==1 && !DeviceInfo.EINK_SCREEN)
-				animatePageFlip(-1, onFinishHandler);
-			else
-				doEngineCommand(cmd, param, onFinishHandler);
+			if (isBookLoaded()) {
+				if (param == 1 && !DeviceInfo.EINK_SCREEN)
+					animatePageFlip(-1, onFinishHandler);
+				else
+					doEngineCommand(cmd, param, onFinishHandler);
+			}
 			break;
 		case DCMD_BEGIN:
 		case DCMD_END:
-			doEngineCommand(cmd, param);
+			if (isBookLoaded())
+				doEngineCommand(cmd, param);
 			break;
 		case DCMD_RECENT_BOOKS_LIST:
 			mActivity.showRecentBooks();
 			break;
 		case DCMD_SEARCH:
-			showSearchDialog(null);
+			if (isBookLoaded())
+				showSearchDialog(null);
 			break;
 		case DCMD_EXIT:
 			mActivity.finish();
 			break;
 		case DCMD_BOOKMARKS:
-			mActivity.showBookmarksDialog();
+			if (isBookLoaded())
+				mActivity.showBookmarksDialog();
 			break;
 		case DCMD_GO_PERCENT_DIALOG:
-			showGoToPercentDialog();
+			if (isBookLoaded())
+				showGoToPercentDialog();
 			break;
 		case DCMD_GO_PAGE_DIALOG:
-			showGoToPageDialog();
+			if (isBookLoaded())
+				showGoToPageDialog();
 			break;
 		case DCMD_TOC_DIALOG:
-			showTOC();
+			if (isBookLoaded())
+				showTOC();
 			break;
 		case DCMD_FILE_BROWSER:
 			mActivity.showBrowser(!mActivity.isBrowserCreated() ? getOpenedFileInfo() : null);
@@ -2471,7 +2488,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 						// do nothing
 						break;
 				}
-				if (isMoveCommand)
+				if (isMoveCommand && isBookLoaded())
 					updateCurrentPositionStatus();
 			}
 			public void done() {
@@ -2479,7 +2496,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 					invalidImages = true;
 					drawPage( doneHandler, false );
 				}
-				if (isMoveCommand)
+				if (isMoveCommand && isBookLoaded())
 			    	scheduleSaveCurrentPositionBookmark(DEF_SAVE_POSITION_INTERVAL);
 			}
 		});
@@ -3380,6 +3397,10 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	private void requestResize(int width, int height) {
 		requestedWidth = width;
 		requestedHeight = height;
+		if (requestedWidth <= 0)
+			requestedWidth = 80;
+		if (requestedHeight <= 0)
+			requestedHeight = 80;
 		checkSize();
 	}
 
@@ -3507,6 +3528,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 //	}
 	private void animatePageFlip( final int dir, final Runnable onFinishHandler )
 	{
+		if (!mOpened)
+			return;
 		BackgroundThread.instance().executeBackground(new Runnable() {
 			@Override
 			public void run() {
@@ -3692,6 +3715,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 
 	private void startAnimation( final int startX, final int startY, final int maxX, final int maxY, final int newX, final int newY )
 	{
+		if (!mOpened)
+			return;
 		alog.d("startAnimation("+startX + ", " + startY+")");
 		BackgroundThread.instance().executeBackground(new Runnable() {
 			@Override
@@ -3756,6 +3781,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	private AnimationUpdate currentAnimationUpdate;
 	private void updateAnimation( final int x, final int y )
 	{
+		if (!mOpened)
+			return;
 		alog.d("updateAnimation("+x + ", " + y+")");
 		synchronized(AnimationUpdate.class) {
 			if (currentAnimationUpdate != null)
@@ -3773,6 +3800,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	
 	private void stopAnimation( final int x, final int y )
 	{
+		if (!mOpened)
+			return;
 		alog.d("stopAnimation("+x+", "+y+")");
 		BackgroundThread.instance().executeBackground(new Runnable() {
 			@Override
@@ -3788,6 +3817,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	DelayedExecutor animationScheduler = DelayedExecutor.createBackground("animation");
 	private void scheduleAnimation()
 	{
+		if (!mOpened)
+			return;
 		animationScheduler.post(new Runnable() {
 			@Override
 			public void run() {
@@ -4615,10 +4646,11 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	}
 	private void drawPage( Runnable doneHandler, boolean isPartially )
 	{
-		if ( !mInitialized || !mOpened )
+		if ( !mInitialized )
 			return;
 		log.v("drawPage() : submitting DrawPageTask");
-		scheduleSaveCurrentPositionBookmark(DEF_SAVE_POSITION_INTERVAL);
+		if ( mOpened )
+			scheduleSaveCurrentPositionBookmark(DEF_SAVE_POSITION_INTERVAL);
 		post( new DrawPageTask(doneHandler, isPartially) );
 	}
 	
@@ -4808,12 +4840,21 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 		public void fail( Exception e )
 		{
 			BackgroundThread.ensureGUI();
+			close();
 			log.v("LoadDocumentTask failed for " + mBookInfo, e);
 			Services.getHistory().removeBookInfo(mActivity.getDB(), mBookInfo.getFileInfo(), true, false );
 			mBookInfo = null;
 			log.d("LoadDocumentTask is finished with exception " + e.getMessage());
 	        mOpened = false;
-			drawPage();
+	        BackgroundThread.instance().executeBackground(new Runnable() {
+		        @Override
+		        public void run() {
+			        doc.createDefaultDocument(mActivity.getString(R.string.error), mActivity.getString(R.string.error_while_opening, filename));
+			        doc.requestRender();
+			        preparePageImage(0);
+			        drawPage();
+		        }
+	        });
 			hideProgress();
 			mActivity.showToast("Error while loading document");
 			if ( errorHandler!=null ) {
@@ -6057,6 +6098,9 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
         this.mEngine = engine;
         surface.setFocusable(true);
         surface.setFocusableInTouchMode(true);
+        // set initial size to exclude java.lang.IllegalArgumentException in Bitmap.createBitmap(0, 0)
+        // surface.getWidth() at this point return 0
+        requestResize(600, 800);
         
         BackgroundThread.instance().postBackground(new Runnable() {
 

crengine/include/lvdocview.h
@@ -727,7 +727,11 @@ public:
     bool docToWindowPoint( lvPoint & pt );
 
     /// returns document
-    ldomDocument * getDocument() { return m_doc; }
+    ldomDocument * getDocument() {
+    	if (NULL == m_doc)
+    	    CRLog::error("attempt to return NULL pointer as document!");
+    	return m_doc;
+    }
     /// return document properties
     CRPropRef getDocProps() { return m_doc_props; }
     /// returns book title

commit a6ee16941dd9bf550785f820ed564fa76d5ccd23

Fixed regression in commit fe9b4ff91c98dd88ccc5d27396597fa49aeb9d85: when opened epub file option "Enable document internal styles" ignored (PROP_EMBEDDED_STYLES). Also fixes issue #81.

android/jni/docview.cpp
@@ -1469,8 +1469,6 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_doCommandInterna
     if ( cmd>=READERVIEW_DCMD_START && cmd<=READERVIEW_DCMD_END) {
     	return p->doCommand(cmd, param)?JNI_TRUE:JNI_FALSE;
     }
-    if (!p->_docview->isDocumentOpened())
-        return JNI_FALSE;
     //CRLog::trace("doCommandInternal(%d, %d) -- passing to LVDocView", cmd, param);
     return p->_docview->doCommand((LVDocCmd)cmd, param) ? JNI_TRUE : JNI_FALSE;
 }

crengine/src/lvdocview.cpp
@@ -5211,6 +5211,10 @@ CRBookmark * LVDocView::findBookmarkByPoint(lvPoint pt) {
 // execute command
 int LVDocView::doCommand(LVDocCmd cmd, int param) {
 	CRLog::trace("doCommand(%d, %d)", (int)cmd, param);
+	if (NULL == m_doc) {
+		CRLog::warn("doCommand(): m_doc is NULL!");
+		return 0;
+	}
 	switch (cmd) {
     case DCMD_SET_DOC_FONTS:
         CRLog::trace("DCMD_SET_DOC_FONTS(%d)", param);