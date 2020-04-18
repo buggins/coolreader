b1fff62c9650e302ac5058774ca546e5726428e7
poire-z <poire-z@users.noreply.github.com>
2020-04-18 10:45:03 +0200

    XML parsing: add more HTML5 named entities, optimize search

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -84,7 +84,7 @@ int gDOMVersionRequested     = DOM_VERSION_CURRENT;
 
 /// change in case of incompatible changes in swap/cache file format to avoid using incompatible swap file
 // increment to force complete reload/reparsing of old file
-#define CACHE_FILE_FORMAT_VERSION "3.12.60"
+#define CACHE_FILE_FORMAT_VERSION "3.12.61"
 
 /// increment following value to force re-formatting of old book after load
 #define FORMATTING_VERSION_ID 0x0021
