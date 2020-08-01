24e743a4624c3dc1261e693e0d08ecca22ddbbee
Aleksey Chernov <valexlin@gmail.com>
2020-08-01 23:25:51 +0400

    Revert "Eliminated erroneous updating of the rendering context immediately after loading the document, which prevented the applying of the embedded document stylesheet."
    This reverts commit 637c80181bdef2a173affa36c6b0711c0308c4cc.

--- crengine/src/lvtinydom.cpp
+++ crengine/src/lvtinydom.cpp
@@ -7481,6 +7481,7 @@ ldomDocumentWriter::~ldomDocumentWriter()
         _document->getRootNode()->initNodeFont();
         //if ( !_document->validateDocument() )
         //    CRLog::error("*** document style validation failed!!!");
+        _document->updateRenderContext();
         _document->dumpStatistics();
         if ( _document->_nodeStylesInvalidIfLoading ) {
             // Some pseudoclass like :last-child has been met which has set this flag
