buggins/coolreader

commit afc16d0838767a9e16f29ce5e4bb98d63e03a7ef

Legacy rendering mode: display block elements that are inside inline elements as block elements.

crengine/src/lvrend.cpp
@@ -2508,7 +2508,10 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
         // - with block nodes (so, only with the first "final" node, and not
         //   when recursing its children which are inline), it will also set
         //   horitontal alignment flags.
-        bool is_block = rm == erm_final;
+        // In legacy rendering mode, we should get the same text formatting flags
+        // as in CoolReader 3.2.38 and earlier, i.e. set is_block to true for
+        // any block elements.
+        bool is_block = BLOCK_RENDERING_G(ENHANCED) ? rm == erm_final : style->display >= css_d_block;
         lUInt32 flags = styleToTextFmtFlags( is_block, style, baseflags, direction );
         // Note:
         // - baseflags (passed by reference) is shared and re-used by this node's siblings
@@ -2628,10 +2631,11 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
             }
         }
 
-        if ((flags & LTEXT_FLAG_NEWLINE) && rm == erm_final) {
-            // Top and single 'final' node (unless in the degenarate case
+        if ( (flags & LTEXT_FLAG_NEWLINE) && ( rm == erm_final || ( !BLOCK_RENDERING_G(ENHANCED) && is_block ) ) ) {
+            // Top and single 'final' node (unless in the degenerate case
             // of obsolete css_d_list_item_legacy):
             // Get text-indent and line-height that will apply to the full final block
+            // There is also an exception: in legacy rendering mode, we must also indent any blocks.
 
             // text-indent should really not have to be handled here: it would be
             // better handled in ldomNode::renderFinalBlock(), grabbing it from the
@@ -2661,20 +2665,22 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
                 // does not need that).
             }
 
-            // We set the LFormattedText strut_height and strut_baseline
-            // with the values from this "final" node. All lines made out from
-            // children will have a minimal height and baseline set to these.
-            // See https://www.w3.org/TR/CSS2/visudet.html#line-height
-            //   The minimum height consists of a minimum height above
-            //   the baseline and a minimum depth below it, exactly as if
-            //   each line box starts with a zero-width inline box with the
-            //   element's font and line height properties. We call that
-            //   imaginary box a "strut."
-            // and https://iamvdo.me/en/blog/css-font-metrics-line-height-and-vertical-align
-            int fh = enode->getFont()->getHeight();
-            int fb = enode->getFont()->getBaseline();
-            int f_half_leading = (line_h - fh) / 2;
-            txform->setStrut(line_h, fb + f_half_leading);
+            if (rm == erm_final) {
+                // We set the LFormattedText strut_height and strut_baseline
+                // with the values from this "final" node. All lines made out from
+                // children will have a minimal height and baseline set to these.
+                // See https://www.w3.org/TR/CSS2/visudet.html#line-height
+                //   The minimum height consists of a minimum height above
+                //   the baseline and a minimum depth below it, exactly as if
+                //   each line box starts with a zero-width inline box with the
+                //   element's font and line height properties. We call that
+                //   imaginary box a "strut."
+                // and https://iamvdo.me/en/blog/css-font-metrics-line-height-and-vertical-align
+                int fh = enode->getFont()->getHeight();
+                int fb = enode->getFont()->getBaseline();
+                int f_half_leading = (line_h - fh) / 2;
+                txform->setStrut(line_h, fb + f_half_leading);
+            }
         }
         else if ( STYLE_HAS_CR_HINT(style, STRUT_CONFINED) ) {
             // Previous branch for the top final node has set the strut.
@@ -3420,19 +3426,23 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
             } else {
             }
             */
-            /* removal of leading spaces is now managed directly by lvtextfm
-            //int offs = 0;
-            if ( (txform->GetSrcCount()==0 || (tflags & LTEXT_IS_LINK)) && style->white_space!=css_ws_pre ) {
-                // clear leading spaces for first text of paragraph
-                int i=0;
-                for ( ;txt.length()>i && (txt[i]==' ' || txt[i]=='\t'); i++ )
-                    ;
-                if ( i>0 ) {
-                    txt.erase(0, i);
-                    //offs = i;
+            if ( !BLOCK_RENDERING_G(ENHANCED) ) {
+                // Removal of leading spaces is now managed directly by lvtextfm
+                // but in legacy render mode we don't add lines with only spaces.
+                //int offs = 0;
+                if ( (txform->GetSrcCount()==0 || (tflags & LTEXT_IS_LINK)) && style->white_space!=css_ws_pre ) {
+                    // clear leading spaces for first text of paragraph
+                    int i=0;
+                    for ( ;txt.length()>i && (txt[i]==' ' || txt[i]=='\t'); i++ )
+                        ;
+                    if ( i>0 ) {
+                        txt.erase(0, i);
+                        //offs = i;
+                    }
                 }
+                // legacy new line processing: set indentation for **each** new line
+                tflags |= LTEXT_LEGACY_RENDERING;
             }
-            */
             if ( txt.length()>0 ) {
                 txform->AddSourceLine( txt.c_str(), txt.length(), cl, bgcl, font.get(), lang_cfg, baseflags | tflags,
                     line_h, valign_dy, indent, enode, 0, letter_spacing );

crengine/include/lvtextfm.h
@@ -81,7 +81,8 @@ extern "C" {
 
 #define LTEXT_FIT_GLYPHS             0x08000000  // Avoid glyph overflows and override at line edges and between text nodes
 
-#define LTEXT__AVAILABLE_BIT_29__    0x10000000
+#define LTEXT_LEGACY_RENDERING       0x10000000  // Legacy rendering exceptions: new line processing: set indentation for **each** new line, etc.
+
 #define LTEXT__AVAILABLE_BIT_30__    0x20000000
 #define LTEXT__AVAILABLE_BIT_31__    0x40000000
 #define LTEXT__AVAILABLE_BIT_32__    0x80000000

crengine/src/lvtextfm.cpp
@@ -3757,10 +3757,15 @@ public:
         while ( pos<m_length ) { // each loop makes a line
             // x is this line indent. We use it like a x coordinates below, but
             // we'll use it on the right in addLine() if para is RTL.
-            int x = m_indent_current;
-            if ( !m_indent_first_line_done ) {
-                m_indent_first_line_done = true;
-                m_indent_current = m_indent_after_first_line;
+            int x;
+            if (para->flags & LTEXT_LEGACY_RENDERING) {
+                x = para->indent > 0 ? (pos == 0 ? para->indent : 0 ) : (pos==0 ? 0 : -para->indent);
+            } else {
+                x = m_indent_current;
+                if ( !m_indent_first_line_done ) {
+                    m_indent_first_line_done = true;
+                    m_indent_current = m_indent_after_first_line;
+                }
             }
             int w0 = pos>0 ? m_widths[pos-1] : 0; // measured cumulative width at start of this line
             int lastNormalWrap = -1;

commit f3477fe7cad452b40fb25565887eb03cb06c68ae

Fixed footnotes rendering in legacy rendering mode that is broken in afc16d0838767a9e16f29ce5e4bb98d63e03a7ef.

crengine/src/lvrend.cpp
@@ -2502,6 +2502,10 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
         int direction = RENDER_RECT_PTR_GET_DIRECTION(fmt);
         bool is_rtl = direction == REND_DIRECTION_RTL;
 
+        ldomNode * parent = enode->getParentNode(); // Needed for various checks below
+        if (parent && parent->isNull())
+            parent = NULL;
+
         // About styleToTextFmtFlags:
         // - with inline nodes, it only updates LTEXT_FLAG_PREFORMATTED flag
         //   when css_ws_pre and LTEXT_FLAG_NOWRAP when css_ws_nowrap.
@@ -2511,7 +2515,24 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
         // In legacy rendering mode, we should get the same text formatting flags
         // as in CoolReader 3.2.38 and earlier, i.e. set is_block to true for
         // any block elements.
-        bool is_block = BLOCK_RENDERING_G(ENHANCED) ? rm == erm_final : style->display >= css_d_block;
+        bool is_block = rm == erm_final;
+        if (!BLOCK_RENDERING_G(ENHANCED) && !is_block) {
+            is_block = style->display >= css_d_block;
+            if (is_block) {
+                // Hack for "legacy" rendering mode:
+                // First node with "display: block" after node "display: run-in" in one section
+                // must be rendered as inline nodes.
+                if ( enode->getNodeIndex() == 1 && parent && parent->getChildCount() > 1 ) {
+                    ldomNode * first_sibling = parent->getChildNode(0);
+                    if (first_sibling && !first_sibling->isNull() && first_sibling->isElement()) {
+                        css_style_ref_t fs_style = first_sibling->getStyle();
+                        if (!fs_style.isNull() && fs_style->display == css_d_run_in) {
+                            is_block = false;
+                        }
+                    }
+                }
+            }
+        }
         lUInt32 flags = styleToTextFmtFlags( is_block, style, baseflags, direction );
         // Note:
         // - baseflags (passed by reference) is shared and re-used by this node's siblings
@@ -2523,9 +2544,6 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
 
         int width = fmt->getWidth();
         int em = enode->getFont()->getSize();
-        ldomNode * parent = enode->getParentNode(); // Needed for various checks below
-        if (parent && parent->isNull())
-            parent = NULL;
 
         // Nodes with "display: run-in" are inline nodes brought at start of the final node
         bool isRunIn = style->display == css_d_run_in;