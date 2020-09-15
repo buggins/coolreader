7e330cff2eb0abef1042277a1d172f1244917556
Aleksey Chernov <valexlin@gmail.com>
2020-09-15 16:07:37 +0400

    Sync with Google Drive: implemented removing obsolete copies of bookmarks from the cloud.

--- android/src/org/coolreader/crengine/BaseActivity.java
+++ android/src/org/coolreader/crengine/BaseActivity.java
@@ -1864,6 +1864,7 @@ public class BaseActivity extends Activity implements Settings {
 			props.applyDefault(ReaderView.PROP_APP_CLOUDSYNC_GOOGLEDRIVE_CURRENTBOOK, "0");
 			props.applyDefault(ReaderView.PROP_APP_CLOUDSYNC_GOOGLEDRIVE_AUTOSAVEPERIOD, "5");		// 5 min.
 			props.applyDefault(ReaderView.PROP_APP_CLOUDSYNC_CONFIRMATIONS, "1");
+			props.applyDefault(ReaderView.PROP_APP_CLOUDSYNC_BOOKMARKS_KEEPALIVE, "14");				// 2 weeks
 
 			if (!DeviceInfo.EINK_SCREEN) {
 				props.applyDefault(ReaderView.PROP_APP_HIGHLIGHT_BOOKMARKS, "1");
