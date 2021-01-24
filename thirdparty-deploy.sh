2437c4741a98f930105c56cf594438a3135414a2
Aleksey Chernov <valexlin@gmail.com>
2021-01-24 19:27:16 +0400

    Added script to deploy sources of thirdparty libraries. Mostly required for building for Android.

---
+++ thirdparty-deploy.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+
+top_srcdir=`pwd`
+
+thirdparty_dir="${top_srcdir}/thirdparty"
+repo_dir="${top_srcdir}/thirdparty_repo"
+repo_tmpdir="${top_srcdir}/thirdparty_tmp"
+
+cleanup()
+{
+	:
+}
+
+die()
+{
+	echo $*
+	cd "${pwd1}"
+	cleanup
+	exit 1
+}
+
+deploy_package()
+{
+	local pkgname="${1}"
+	local metafile="${repo_dir}/libunibreak.meta.sh"
+
+	source "${metafile}"
+
+	local patchesdir="${repo_dir}/patches/${PN}"
+	local pkg_datadir="${repo_tmpdir}/${PN}"
+
+	local pwd1=`pwd`
+
+	if [ ! -d "${pkg_datadir}" ]
+	then
+		mkdir "${pkg_datadir}" || die "Failed to create package tmpdir!"
+	fi
+	cd "${pkg_datadir}" || die "chdir failed!"
+
+	# Check consistency
+	if [ ! -d "${thirdparty_dir}/${SOURCESDIR}" ]
+	then
+		rm -f .unpacked .prepared
+	fi
+	if [ ! -f "${SRCFILE}" ]
+	then
+		rm -f .downloaded .verified
+	fi
+
+	if [ ! -f .downloaded ]
+	then
+		if [ -f "${SRCFILE}" ]
+		then
+			rm -f "${SRCFILE}"
+		fi
+		curl -L -O ${URL} || die "Failed to fetch sources!"
+		if [ ! -f "${SRCFILE}" ]
+		then
+			die "Something wrong... source file not found!"
+		fi
+		echo "1" > .downloaded
+		echo "Downloaded OK."
+	fi
+
+	if [ ! -f .verified ]
+	then
+		# make sha512 sum file
+		echo "${SHA512} *${SRCFILE}" > "${SRCFILE}.sha512"
+		sha512sum -c "${SRCFILE}.sha512" || die "Failed to verify checksum!"
+		echo "1" > .verified
+		rm -f "${SRCFILE}.sha512"
+		echo "Checksum OK."
+	fi
+
+	if [ ! -f .unpacked ]
+	then
+		cd "${thirdparty_dir}" || die "chdir to thirdparty_dir failed!"
+		tar -xzf "${pkg_datadir}/${SRCFILE}" || die "Failed to unpack sources!"
+		cd "${pkg_datadir}" || die "chdir failed!"
+		echo "1" > .unpacked
+		echo "Unpacked OK."
+	fi
+
+	if [ ! -f .prepared ]
+	then
+		cd "${thirdparty_dir}/${SOURCESDIR}" || die "chdir to srcdir failed!"
+		for p in ${PATCHES}
+		do
+			patch -p1 -i "${patchesdir}/${p}" || die "Failed to patch!"
+		done
+		cd "${pkg_datadir}" || die "chdir failed!"
+		echo "1" > .prepared
+		echo "Prepared OK."
+	fi
+
+
+	# clean vars
+	unset -v PN
+	unset -v PV
+	unset -v SRCFILE
+	unset -v SHA512
+	unset -v URL
+	unset -v SOURCEDIR
+	unset -v PATCHES
+
+	cd "${pwd1}"
+}
+
+# check current directory
+if [ ! -f ./thirdparty-deploy.sh ]
+then
+	die "Error! You must call this script only in top_srcdir!"
+fi
+
+if [ ! -d "${thirdparty_dir}" ]
+then
+	mkdir "${thirdparty_dir}" || die "Failed to create thirdparty_dir!"
+fi
+
+if [ ! -d "${repo_tmpdir}" ]
+then
+	mkdir "${repo_tmpdir}" || die "Failed to create repo tmpdir!"
+fi
+
+deploy_package libunibreak
