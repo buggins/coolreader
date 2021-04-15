1617315d4d9d1a4b7ed4e4b5116908ff5cc3b9c6
Aleksey Chernov <valexlin@gmail.com>
2021-04-16 00:09:07 +0400

    Added intermediate values for property PROP_INTERLINE_SPACE in program setings dialog.

--- android/src/org/coolreader/crengine/OptionsDialog.java
+++ android/src/org/coolreader/crengine/OptionsDialog.java
@@ -103,7 +103,12 @@ public class OptionsDialog extends BaseDialog implements TabContentFactory, Opti
 	public static int[] mPagesPerFullSwipe;
 	public static String[] mPagesPerFullSwipeTitles;
 	int[] mInterlineSpaces = new int[] {
-			80, 85, 90, 95, 100, 105, 110, 115, 120, 130, 140, 150, 160, 180, 200
+			 80,  81,  82,  83,  84,  85, 86,   87,  88,  89,
+			 90,  91,  92,  93,  94,  95, 96,   97,  98,  99,
+			100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+			110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+			120, 125, 130, 135, 140, 145, 150, 155, 160, 165,
+			170, 175, 180, 185, 190, 195, 200
 		};
 	int[] mMinSpaceWidths = new int[] {
 			25, 30, 40, 50, 60, 70, 80, 90, 100
