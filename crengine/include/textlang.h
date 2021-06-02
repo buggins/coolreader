buggins/coolreader

commit af7872641ca64bc305c1c872c44afa04dca4847d

CSS: add support for 'line-break' and 'word-break'

crengine/include/textlang.h
@@ -127,6 +127,7 @@ class TextLangCfg
     #endif
 
     bool _duplicate_real_hyphen_on_next_line;
+    bool _is_ja_zh;
 
     void resetCounters();
 
@@ -163,6 +164,8 @@ public:
     bool hasLBCharSubFunc() const { return _lb_char_sub_func != NULL; }
     lb_char_sub_func_t getLBCharSubFunc() const { return _lb_char_sub_func; }
     struct LineBreakProperties * getLBProps() const { return (struct LineBreakProperties *)_lb_props; }
+    lChar32 getCssLbCharSub(css_line_break_t css_linebreak, css_word_break_t css_wordbreak,
+                struct LineBreakContext *lbpCtx, const lChar32 * text, int pos, int next_usable, lChar32 tweaked_ch);
     #endif
 
     bool duplicateRealHyphenOnNextLine() const { return _duplicate_real_hyphen_on_next_line; }

crengine/src/textlang.cpp
@@ -7,6 +7,10 @@
 #include "../include/fb2def.h"
 #include "../include/crlog.h"
 
+#if (USE_UTF8PROC==1)
+#include <utf8proc.h>
+#endif
+
 // Uncomment to see which lang_tags are seen and lang_cfg created
 // #define DEBUG_LANG_USAGE
 
@@ -694,6 +698,146 @@ lChar32 lb_char_sub_func_czech_slovak(struct LineBreakContext *lbpCtx, const lCh
     }
     return text[pos];
 }
+
+// (Mostly) non-language specific char substitution to ensure CSS line-break and word-break properties
+//
+// Note: the (hardcoded in many places) default behaviour (without these tweaks) in crengine
+// resembles "word-break: break-word; overflow-wrap: break-word", that is: we don't let text
+// overflow their container. So, we don't differentiate "word-break: normal" from "break-word",
+// and we don't handle "overflow-wrap" (normal/break-word/anywhere).
+//
+// Specs:
+//   https://drafts.csswg.org/css-text-3/#word-break-property
+//   https://drafts.csswg.org/css-text-3/#line-break-property
+// Also see:
+//   https://florian.rivoal.net/talks/line-breaking/
+// Rust implementation of UAX#14 and these CSS properties:
+//   https://github.com/makotokato/uax14_rs
+// WebKit:
+//   https://bugs.webkit.org/show_bug.cgi?id=89235
+//   https://trac.webkit.org/changeset/176473/webkit
+//   https://trac.webkit.org/wiki/LineBreakingCSS3Mapping
+// Firefox:
+//   https://bugzilla.mozilla.org/show_bug.cgi?id=1011369
+//   https://bugzilla.mozilla.org/show_bug.cgi?id=1531715
+//   https://hg.mozilla.org/integration/autoland/rev/436e3199c386
+//   https://hg.mozilla.org/releases/mozilla-esr78/file/tip/intl/lwbrk/LineBreaker.cpp
+//
+// Most of these implementation handles line breaking themselves, which we do not
+// exactly do here: we just masquerade an original char with another one with
+// different line-breaking properties, before invoking libunibreak pure UAX#14
+// implementation, expecting this to exhibit the wished behaviour.
+// Below, we mostly always use a random LBP_ID (CJK ideographic char) that should
+// break on both sides: this allows breaking on the unbreakable side, but it may
+// change the original LB class behaviour on the other side...
+//
+lChar32 TextLangCfg::getCssLbCharSub(css_line_break_t css_linebreak, css_word_break_t css_wordbreak,
+                struct LineBreakContext *lbpCtx, const lChar32 * text, int pos, int next_usable, lChar32 tweaked_ch) {
+    // "line-break: anywhere" has precedence over everything
+    if ( css_linebreak == css_lb_anywhere ) {
+        return 0x5000; // Random CJK ideographic character LBP_ID
+                       // Everything becoming ID, it can break anywhere
+    }
+    lChar32 ch = tweaked_ch ? tweaked_ch : text[pos];
+    enum LineBreakClass lbc = lb_get_char_class(lbpCtx, ch);
+    if ( css_wordbreak == css_wb_break_all ) {
+        if ( lbc == LBP_AI || lbc == LBP_AL || lbc == LBP_NU || lbc == LBP_SA ) {
+            // Alphabetic, ambiguous (ID in CJK lang, AL otherwise) numeric, south-east asian:
+            // treated as ideographic
+            // (Note: Firefox includes others: CJ H2 H3 JL JT JV
+            return 0x5000; // Random CJK ideographic character LBP_ID
+        }
+    }
+    else if ( css_wordbreak == css_wb_keep_all ) {
+	// A char of classes AI AL ID NU HY H2 H3 JL JV JT CJ shouldn't break
+        // between another char of any of these class.
+        // (Note: uax14_rs includes LBP_HY but Firefox does not)
+        // Feels like treating a char of these classes just as AL
+        if ( lbc == LBP_AI || lbc == LBP_AL || lbc == LBP_ID || lbc == LBP_NU || lbc == LBP_HY || lbc == LBP_H2 ||
+                    lbc == LBP_H3 || lbc == LBP_JL || lbc == LBP_JV || lbc == LBP_JT || lbc == LBP_CJ ) {
+            return 0x41; // 'A' LBP_AL
+        }
+    }
+    if ( css_linebreak > css_lb_auto ) {
+        // Following rules from https://drafts.csswg.org/css-text-3/#line-break-property:
+
+        // "The following breaks are forbidden in strict line breaking and allowed in normal and loose:
+        // - breaks before Japanese small kana or the Katakana-Hiragana prolonged sound mark,
+        // i.e. character from the Unicode line breaking class CJ [UAX14]."
+        if ( css_linebreak == css_lb_strict && lbc == LBP_CJ) {
+            // Conditional Japanese Starter => Nonstarter (as done by libunibreak itself
+            // when the lang tag ends with "-strict')
+            return 0x2047; // '⁇' LBP_NS
+        }
+
+        // "The following breaks are allowed for normal and loose line breaking if the writing
+        // system is Chinese or Japanese, and are otherwise forbidden:
+        // - breaks before certain CJK hyphen-like characters: U+301C,  U+30A0"
+        if ( _is_ja_zh && css_linebreak != css_lb_strict && ( ch==0x301C || ch==0x30A0 ) ) {
+            // By default, libunibreak considers them LBP_NS (Nonstarter, non breakable before).
+            // https://unicode.org/reports/tr14/#NS: "Optionally, the NS restriction may be relaxed
+            // by tailoring, with some or all characters treated like ID to achieve a more permissive
+            // style of line breaking, especially in some East Asian document styles"
+            return 0x5000; // Random CJK ideographic character LBP_ID
+        }
+
+        // "The following breaks are allowed for loose line breaking if the preceding character
+        // belongs to the Unicode line breaking class ID [UAX14] (including when the preceding
+        // character is treated as ID due to word-break: break-all), and are otherwise forbidden:
+        // - breaks before hyphens: U+2010, U+2013"
+        if ( css_linebreak == css_lb_loose && pos > 1 && ( ch==0x2010 || ch==0x2013 ) ) {
+            // By default, libunibreak considers them LBP_NS (Nonstarter, non breakable before).
+            enum LineBreakClass plbc = lb_get_char_class(lbpCtx, text[pos-1]);
+            if ( plbc == LBP_ID || (css_wordbreak == css_wb_break_all &&
+                                    ( plbc == LBP_AI || plbc == LBP_AL || plbc == LBP_NU || plbc == LBP_SA )) ) {
+                return 0x5000; // Random CJK ideographic character LBP_ID
+            }
+        }
+
+        // "The following breaks are forbidden for normal and strict line breaking and allowed in loose:
+        // - breaks before iteration marks: U+3005, U+303B, U+309D, U+309E, U+30FD, U+30FE
+        // - breaks between inseparable characters (such as U+2025, U+2026) i.e. characters
+        //   from the Unicode line breaking class IN [UAX14]. "
+        if ( css_linebreak == css_lb_loose && ( ch==0x3005 || ch==0x303B || ch==0x309D ||
+                                                ch==0x309E || ch==0x30FD || ch==0x30FE ||
+                                                lbc == LBP_IN ) ) {
+            // By default, libunibreak considers these codepoints LBP_NS (Nonstarter, non breakable before).
+            // LBP_IN are inseparable from other of the same class, so making them ID should allow breaking.
+            return 0x5000; // Random CJK ideographic character LBP_ID
+        }
+
+        // "The following breaks are allowed for loose if the writing system is Chinese or Japanese
+        // and are otherwise forbidden:
+        // - breaks before certain centered punctuation marks: U+30FB, U+FF1A, U+FF1B, U+FF65, U+203C,
+        //   U+2047, U+2048, U+2049, U+FF01, U+FF1F
+        // - breaks before suffixes: Characters with the Unicode line breaking class PO [UAX14]
+        //   and the East Asian Width property [UAX11] Ambiguous, Fullwidth, or Wide.
+        // - breaks after prefixes: Characters with the Unicode line breaking class PR [UAX14]
+        //   and the East Asian Width property [UAX11] Ambiguous, Fullwidth, or Wide."
+        if ( _is_ja_zh && css_linebreak == css_lb_loose ) {
+            // By default, libunibreak considers these codepoints LBP_NS (Nonstarter, non breakable before)
+            // or (the last 2 ones) LBP_EX (Exclamation, Interrogation, Prohibit line breaks before)
+            if ( ch==0x30FB || ch==0xFF1A || ch==0xFF1B || ch==0xFF65 ||
+                 ch==0x203C || ch==0x2047 || ch==0x2048 || ch==0x2049 ||
+                 ch==0xFF01 || ch==0xFF1F ) {
+                return 0x5000; // Random CJK ideographic character LBP_ID
+            }
+            // For the 2 last cases, we need utf8proc to know the East Asian Width property
+            #if (USE_UTF8PROC==1)
+            if ( lbc == LBP_PO || lbc == LBP_PR ) {
+                // LBP_PO are Postfix Numeric ("do not break following a numeric expression")
+                // LBP_PR are Prefix Numeric ("do not break in front of a numeric expression")
+                if ( utf8proc_charwidth(ch) == 2 ) {
+                    // Note: utf8proc returns 2 for Fullwidth and Wide, 1 or 0 otherwise.
+                    // It may return 1 for "Ambiguous", so we may not handle this case well.
+                    return 0x5000; // Random CJK ideographic character LBP_ID
+                }
+            }
+            #endif
+        }
+    }
+    return ch;
+}
 #endif
 
 // Instantiate a new TextLangCfg with properties adequate to the provided lang_tag
@@ -747,6 +891,9 @@ TextLangCfg::TextLangCfg( lString32 lang_tag ) {
     // https://unicode.org/reports/tr14/#Hyphen : in Polish and Portuguese,
     // a real hyphen at end of line must be duplicated at start of next line.
     _duplicate_real_hyphen_on_next_line = false;
+
+    // getCssLbCharSub(), possibly called on each glyph, has some different behaviours with 'ja' and 'zh'
+    _is_ja_zh = LANG_STARTS_WITH(("ja") ("zh"));
 
 #if USE_HARFBUZZ==1
     _hb_language = hb_language_from_string(UnicodeToLocal(hb_lang_tag).c_str(), -1);

crengine/include/cssdef.h
@@ -340,6 +340,25 @@ enum css_visibility_t {
     css_v_collapse // handled as "hidden" (but it should behave differently on tables, which we don't ensure)
 };
 
+/// line-break property values
+enum css_line_break_t {
+    css_lb_inherit,
+    css_lb_auto,
+    css_lb_normal,
+    css_lb_loose,
+    css_lb_strict,
+    css_lb_anywhere
+};
+
+/// word-break property values
+enum css_word_break_t {
+    css_wb_inherit,
+    css_wb_normal,
+    css_wb_break_word, // legacy, handled as "normal" (and "overflow-wrap: anywhere", which is our default behaviour)
+    css_wb_break_all,
+    css_wb_keep_all
+};
+
 enum css_generic_value_t {
     css_generic_auto = -1,        // (css_val_unspecified, css_generic_auto), for "margin: auto"
     css_generic_normal = -2,      // (css_val_unspecified, css_generic_normal), for "line-height: normal"

crengine/include/lvstyles.h
@@ -90,10 +90,12 @@ enum css_style_rec_important_bit {
     imp_bit_clear,
     imp_bit_direction,
     imp_bit_visibility,
+    imp_bit_line_break,
+    imp_bit_word_break,
     imp_bit_content,
     imp_bit_cr_hint
 };
-#define NB_IMP_BITS 66 // The number of lines in the enum above: KEEP IT UPDATED.
+#define NB_IMP_BITS 68 // The number of lines in the enum above: KEEP IT UPDATED.
 
 #define NB_IMP_SLOTS    ((NB_IMP_BITS-1)>>5)+1
 // In lvstyles.cpp, we have hardcoded important[0] ... importance[1]
@@ -170,6 +172,8 @@ struct css_style_rec_tag {
     css_clear_t            clear;
     css_direction_t        direction;
     css_visibility_t       visibility;
+    css_line_break_t       line_break;
+    css_word_break_t       word_break;
     lString32              content;
     css_length_t           cr_hint;
     // The following should only be used when applying stylesheets while in lvend.cpp setNodeStyle(),
@@ -225,6 +229,8 @@ struct css_style_rec_tag {
     , clear(css_c_none)
     , direction(css_dir_inherit)
     , visibility(css_v_inherit)
+    , line_break(css_lb_inherit)
+    , word_break(css_wb_inherit)
     , cr_hint(css_val_inherited, 0)
     , flags(0)
     , pseudo_elem_before_style(NULL)

crengine/include/lvtextfm.h
@@ -91,7 +91,9 @@ extern "C" {
 // mostly used for rare inherited CSS properties that don't need us to waste a bit for
 // them in the above flags. The LTEXT_HAS_EXTRA signals one or more of these are set.
 enum ltext_extra_t {
-    LTEXT_EXTRA_CSS_HIDDEN = 1          // visibility: hidden
+    LTEXT_EXTRA_CSS_HIDDEN = 1,         // visibility: hidden
+    LTEXT_EXTRA_CSS_LINE_BREAK,         // line-break: anywhere, or loose/normal/strict (for lang=ja/zh)
+    LTEXT_EXTRA_CSS_WORD_BREAK,         // word-break: break-all or keep-all
 };
 
 /** \brief Source text line

crengine/src/lvrend.cpp
@@ -3204,6 +3204,12 @@ void renderFinalBlock( ldomNode * enode, LFormattedText * txform, RenderRectAcce
         if ( style->visibility >= css_v_hidden ) { // hidden
             flags |= LTEXT_HAS_EXTRA;
         }
+        if ( style->line_break > css_lb_auto ) { // normal, loose, strict
+            flags |= LTEXT_HAS_EXTRA;
+        }
+        if ( style->word_break > css_wb_break_word ) { // break-all or keep-all (break-word is handled as normal)
+            flags |= LTEXT_HAS_EXTRA;
+        }
 
         // Now, process styles that may differ between inline nodes, and
         // are needed to display any children text node.
@@ -4065,6 +4071,8 @@ void copystyle( css_style_ref_t source, css_style_ref_t dest )
     dest->clear = source->clear;
     dest->direction = source->direction;
     dest->visibility = source->visibility;
+    dest->line_break = source->line_break;
+    dest->word_break = source->word_break;
     dest->content = source->content ;
     dest->cr_hint.type = source->cr_hint.type ;
     dest->cr_hint.value = source->cr_hint.value ;
@@ -9850,6 +9858,8 @@ void setNodeStyle( ldomNode * enode, css_style_ref_t parent_style, LVFontRef par
     UPDATE_STYLE_FIELD( list_style_type, css_lst_inherit );
     UPDATE_STYLE_FIELD( list_style_position, css_lsp_inherit );
     UPDATE_STYLE_FIELD( visibility, css_v_inherit );
+    UPDATE_STYLE_FIELD( line_break, css_lb_inherit );
+    UPDATE_STYLE_FIELD( word_break, css_wb_inherit );
 
     // Note: we don't inherit "direction" (which should be inherited per specs);
     // We'll handle inheritance of direction in renderBlockEnhanced, because

crengine/src/lvstsheet.cpp
@@ -124,6 +124,10 @@ enum css_decl_code {
     cssd_clear,
     cssd_direction,
     cssd_visibility,
+    cssd_line_break,
+    cssd_line_break2,
+    cssd_line_break3,
+    cssd_word_break,
     cssd_content,
     cssd_cr_ignore_if_dom_version_greater_or_equal,
     cssd_cr_hint,
@@ -227,6 +231,10 @@ static const char * css_decl_name[] = {
     "clear",
     "direction",
     "visibility",
+    "line-break",
+    "-epub-line-break",
+    "-webkit-line-break",
+    "word-break",
     "content",
     "-cr-ignore-if-dom-version-greater-or-equal",
     "-cr-hint",
@@ -1841,6 +1849,29 @@ static const char * css_v_names[] =
     NULL
 };
 
+// line-break value names
+static const char * css_lb_names[] =
+{
+    "inherit",
+    "auto",
+    "normal",
+    "loose",
+    "strict",
+    "anywhere",
+    NULL
+};
+
+// word-break value names
+static const char * css_wb_names[] =
+{
+    "inherit",
+    "normal",
+    "break-word",
+    "break-all",
+    "keep-all",
+    NULL
+};
+
 static const char * css_cr_only_if_names[]={
         "any",
         "always",
@@ -2994,6 +3025,15 @@ bool LVCssDeclaration::parse( const char * &decl, lUInt32 domVersionRequested, b
             case cssd_visibility:
                 n = parse_name( decl, css_v_names, -1 );
                 break;
+            case cssd_line_break:
+            case cssd_line_break2:
+            case cssd_line_break3:
+                prop_code = cssd_line_break;
+                n = parse_name( decl, css_lb_names, -1 );
+                break;
+            case cssd_word_break:
+                n = parse_name( decl, css_wb_names, -1 );
+                break;
             case cssd_content:
                 {
                     lString32 parsed_content;
@@ -3310,6 +3350,12 @@ void LVCssDeclaration::apply( css_style_rec_t * style )
             break;
         case cssd_visibility:
             style->Apply( (css_visibility_t) *p++, &style->visibility, imp_bit_visibility, is_important );
+            break;
+        case cssd_line_break:
+            style->Apply( (css_line_break_t) *p++, &style->line_break, imp_bit_line_break, is_important );
+            break;
+        case cssd_word_break:
+            style->Apply( (css_word_break_t) *p++, &style->word_break, imp_bit_word_break, is_important );
             break;
         case cssd_cr_hint:
             {

crengine/src/lvstyles.cpp
@@ -45,7 +45,7 @@ lUInt32 calcHash(font_ref_t & f)
 lUInt32 calcHash(css_style_rec_t & rec)
 {
     if ( !rec.hash )
-        rec.hash = (((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
+        rec.hash = (((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
          + (lUInt32)rec.important[0]) * 31
          + (lUInt32)rec.important[1]) * 31
          + (lUInt32)rec.important[2]) * 31
@@ -113,6 +113,8 @@ lUInt32 calcHash(css_style_rec_t & rec)
          + (lUInt32)rec.clear) * 31
          + (lUInt32)rec.direction) * 31
          + (lUInt32)rec.visibility) * 31
+         + (lUInt32)rec.line_break) * 31
+         + (lUInt32)rec.word_break) * 31
          + (lUInt32)rec.cr_hint.pack()) * 31
          + (lUInt32)rec.font_name.getHash()
          + (lUInt32)rec.background_image.getHash()
@@ -190,6 +192,8 @@ bool operator == (const css_style_rec_t & r1, const css_style_rec_t & r2)
            r1.clear == r2.clear&&
            r1.direction == r2.direction&&
            r1.visibility == r2.visibility&&
+           r1.line_break == r2.line_break&&
+           r1.word_break == r2.word_break&&
            r1.content == r2.content&&
            r1.cr_hint==r2.cr_hint;
 }
@@ -388,6 +392,8 @@ bool css_style_rec_t::serialize( SerialBuf & buf )
     ST_PUT_ENUM(clear);
     ST_PUT_ENUM(direction);
     ST_PUT_ENUM(visibility);
+    ST_PUT_ENUM(line_break);
+    ST_PUT_ENUM(word_break);
     buf << content;
     ST_PUT_LEN(cr_hint);
     lUInt32 hash = calcHash(*this);
@@ -458,6 +464,8 @@ bool css_style_rec_t::deserialize( SerialBuf & buf )
     ST_GET_ENUM(css_clear_t, clear);
     ST_GET_ENUM(css_direction_t, direction);
     ST_GET_ENUM(css_visibility_t, visibility);
+    ST_GET_ENUM(css_line_break_t, line_break);
+    ST_GET_ENUM(css_word_break_t, word_break);
     buf>>content;
     ST_GET_LEN(cr_hint);
     lUInt32 hash = 0;

commit fc18c9d9ff95a3216c2513b5b6d7be9b0d394e89

Compile errors fixed that introduced in af7872641ca64bc305c1c872c44afa04dca4847d.

crengine/include/textlang.h
@@ -4,6 +4,7 @@
 #include "crsetup.h"
 #include "lvptrvec.h"
 #include "lvstring.h"
+#include "cssdef.h"
 
 #if USE_HARFBUZZ==1
 #include <hb.h>

crengine/src/lvimg/clzwdecoder.cpp
@@ -16,7 +16,7 @@
 
 #if (USE_GIF==1)
 
-#include <cstddef>
+#include <stddef.h>
 
 void CLZWDecoder::SetInputStream(const unsigned char *p, int sz) {
     p_in_stream = p;

crengine/src/textlang.cpp
@@ -739,6 +739,7 @@ lChar32 TextLangCfg::getCssLbCharSub(css_line_break_t css_linebreak, css_word_br
                        // Everything becoming ID, it can break anywhere
     }
     lChar32 ch = tweaked_ch ? tweaked_ch : text[pos];
+#if KO_LIBUNIBREAK_PATCH==1
     enum LineBreakClass lbc = lb_get_char_class(lbpCtx, ch);
     if ( css_wordbreak == css_wb_break_all ) {
         if ( lbc == LBP_AI || lbc == LBP_AL || lbc == LBP_NU || lbc == LBP_SA ) {
@@ -836,9 +837,10 @@ lChar32 TextLangCfg::getCssLbCharSub(css_line_break_t css_linebreak, css_word_br
             #endif
         }
     }
+#endif  // KO_LIBUNIBREAK_PATCH==1
     return ch;
 }
-#endif
+#endif  // USE_LIBUNIBREAK==1
 
 // Instantiate a new TextLangCfg with properties adequate to the provided lang_tag
 TextLangCfg::TextLangCfg( lString32 lang_tag ) {