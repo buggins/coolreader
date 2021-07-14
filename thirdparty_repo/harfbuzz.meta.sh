ad43e114c702d99cd99a38b2dabc12bf974a6ab5
Aleksey Chernov <valexlin@gmail.com>
2021-07-14 22:47:34 +0400

    Thirdparty repo: bump HarfBuzz to 2.8.2 version.

--- thirdparty_repo/harfbuzz.meta.sh
+++ thirdparty_repo/harfbuzz.meta.sh
@@ -2,12 +2,12 @@
 # Metadata for deploy script
 
 PN="harfbuzz"
-PV="2.8.0"
+PV="2.8.2"
 # package revision: when patchset is changed (but not version), increase it
 # when version changed, reset to "1".
 REV="1"
 SRCFILE="${PN}-${PV}.tar.xz"
-SHA512="0ece78fa5c18be1fd693f40493ff3689a297ef4d5ad82f31054e8f42c1ba4e8b93afe1ed1ccb9eb9a0f165f8d1343c12c0606f1768a4f85bb041ae65018e6d83"
+SHA512="47f672967ba9e9a1ea21164b9780cbaaad2f4f619811e84d1b6bdb598e5ad60c96e08fadd270460a4ce8e2fb4603fcdd881ba55f2826abc2a4ab439efdc856dc"
 
 URL="https://github.com/harfbuzz/harfbuzz/releases/download/${PV}/${SRCFILE}"
 
