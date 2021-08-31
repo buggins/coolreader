buggins/coolreader

commit 4b98d13dc3b828f3749bfe3c9a459610c91b3ab3

Android: When opening an external stream, find the corresponding file in the database to find the last read position and load the saved bookmarks, and if not found, save it to the application's private directory and save the information to the database.

android/jni/docview.cpp
@@ -877,12 +877,12 @@ static DocViewNative * getNative(JNIEnv * env, jobject _this)
 	return res;
 }
 
-void DocViewNative::createDefaultDocument( lString32 title, lString32 message )
+void DocViewNative::createDefaultDocument( const lString32& title, const lString32& message )
 {
 	_docview->createDefaultDocument(title, message);
 }
 
-bool DocViewNative::loadDocument( lString32 filename )
+bool DocViewNative::loadDocument( const lString32& filename )
 {
 	CRLog::info("Loading document %s", LCSTR(filename));
 	bool res = _docview->LoadDocument(filename.c_str());
@@ -901,7 +901,7 @@ bool DocViewNative::loadDocument( lString32 filename )
     return res;
 }
 
-bool DocViewNative::loadDocument( LVStreamRef stream, lString32 contentPath )
+bool DocViewNative::loadDocument( LVStreamRef stream, const lString32& contentPath )
 {
 	CRLog::info("Loading document from memory stream, content path: %s", LCSTR(contentPath));
 	bool res = _docview->LoadDocument(stream, contentPath.c_str(), false);
@@ -1549,8 +1549,8 @@ JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_DocView_loadDocumentFrom
 	}
 	DocViewCallback callback( _env, p->_docview, _this );
 	LVStreamRef stream = env.jbyteArrayToStream(buf);
-	lString32 contentPath16 = env.fromJavaString(contentPath);
-	bool res = p->loadDocument(stream, contentPath16);
+	lString32 contentPath32 = env.fromJavaString(contentPath);
+	bool res = p->loadDocument(stream, contentPath32);
 	return res ? JNI_TRUE : JNI_FALSE;
 }
 
@@ -1790,10 +1790,10 @@ JNIEXPORT jobject JNICALL Java_org_coolreader_crengine_DocView_getCurrentPageBoo
 /*
  * Class:     org_coolreader_crengine_DocView
  * Method:    updateBookInfoInternal
- * Signature: (Lorg/coolreader/crengine/BookInfo;)V
+ * Signature: (Lorg/coolreader/crengine/BookInfo;Z)V
  */
 JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_updateBookInfoInternal
-  (JNIEnv * _env, jobject _this, jobject _info)
+  (JNIEnv * _env, jobject _this, jobject _info, jboolean _updatePath)
 {
 	CRJNIEnv env(_env);
     DocViewNative * p = getNative(_env, _this);
@@ -1838,6 +1838,43 @@ JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_updateBookInfoIntern
     descriptionField.set(p->_docview->getDescription());
     CRLongField crc32Field(fileinfo,"crc32");
     crc32Field.set((lInt64)p->_docview->getFileCRC32());
+    jclass clazz_docformat = env->FindClass("org/coolreader/crengine/DocumentFormat");
+    jmethodID methodId_byId = NULL;
+    if (NULL != clazz_docformat) {
+        methodId_byId = env->GetStaticMethodID(clazz_docformat, "byId", "(I)Lorg/coolreader/crengine/DocumentFormat;");
+    }
+    if (NULL != methodId_byId) {
+        jobject jformat = env->CallStaticObjectMethod(clazz_docformat, methodId_byId, p->_docview->getDocFormat());
+        CRObjectField formatField(fileinfo, "format", "Lorg/coolreader/crengine/DocumentFormat;");
+        formatField.set(jformat);
+    }
+    if (_updatePath) {
+		CRPropRef doc_props = p->_docview->getDocProps();
+		if (!doc_props.isNull()) {
+			bool isArchive = !doc_props->getStringDef(DOC_PROP_ARC_NAME, "").empty();
+			if (isArchive) {
+				lString32 arcname = doc_props->getStringDef(DOC_PROP_ARC_NAME, "");
+				int arcsize = doc_props->getIntDef(DOC_PROP_ARC_SIZE, 0);
+				CRStringField arcnameField(fileinfo, "arcname");
+				arcnameField.set(arcname);
+				CRLongField arcsizeField(fileinfo, "arcsize");
+				arcsizeField.set(arcsize);
+			}
+			lString32 pathname = doc_props->getStringDef(DOC_PROP_FILE_NAME, "");
+			lString32 filename = LVExtractFilename(pathname);
+			int size = doc_props->getIntDef(DOC_PROP_FILE_SIZE, 0);
+			CRBooleanField isArchiveField(fileinfo, "isArchive");
+			isArchiveField.set(isArchive);
+			CRStringField pathnameField(fileinfo, "pathname");
+			CRStringField pathField(fileinfo, "path");
+			pathnameField.set(pathname);
+			pathField.set(pathname);
+			CRStringField filenameField(fileinfo, "filename");
+			filenameField.set(filename);
+			CRLongField sizeField(fileinfo, "size");
+			sizeField.set(size);
+		}
+	}
 }
 
 /*

android/src/org/coolreader/crengine/ReaderView.java
@@ -31,6 +31,8 @@ import org.coolreader.R;
 import org.coolreader.crengine.InputDialog.InputHandler;
 import org.koekak.android.ebookdownloader.SonyBookSelector;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -2659,7 +2661,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	private void updateLoadedBookInfo() {
 		BackgroundThread.ensureBackground();
 		// get title, authors, genres, etc.
-		doc.updateBookInfo(mBookInfo);
+		doc.updateBookInfo(mBookInfo, true);
 		updateCurrentPositionStatus();
 		// check whether current book properties updated on another devices
 		// TODO: fix and reenable
@@ -3086,26 +3088,47 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	}
 
 	public boolean loadDocumentFromStream(final InputStream inputStream, final FileInfo fileInfo, final Runnable doneHandler, final Runnable errorHandler) {
-		log.v("loadDocument(" + fileInfo.getPathName() + ")");
-		if (this.mBookInfo != null && this.mBookInfo.getFileInfo().pathname.equals(fileInfo.pathname) && mOpened) {
-			log.d("trying to load already opened document");
-			mActivity.showReader();
-			if (null != doneHandler)
-				doneHandler.run();
-			drawPage();
-			return false;
+		log.v("loadDocumentFromStream(" + fileInfo.getPathName() + ")");
+		// When the document is opened from the stream at this moment,
+		// we do not know the real path to the file, since it will be
+		// changed after the successful opening of the document,
+		// so here we cannot compare the path to the document currently
+		// open with the fileinfo argument.
+
+		// Copy data from input stream to byte array
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		boolean copyOk = false;
+		try {
+			byte [] buf = new byte [4096];
+			int readBytes;
+			while (true) {
+				readBytes = inputStream.read(buf);
+				if (readBytes > 0)
+					outputStream.write(buf, 0, readBytes);
+				else
+					break;
+			}
+			copyOk = true;
+		} catch (IOException e1) {
+			log.e("I/O error while copying content from input stream to buffer. Interrupted.");
+		} catch (OutOfMemoryError e2) {
+			log.e("Out of memory while copying content from input stream to buffer. Interrupted.");
 		}
-		Services.getHistory().getOrCreateBookInfo(mActivity.getDB(), fileInfo, bookInfo -> {
+		if (copyOk) {
+			byte[] docBuffer = outputStream.toByteArray();
+			// Don't search in DB this memory stream before opening it
+			BookInfo bookInfo = new BookInfo(fileInfo);
 			log.v("posting LoadDocument task to background thread");
 			BackgroundThread.instance().postBackground(() -> {
 				log.v("posting LoadDocument task to GUI thread");
 				BackgroundThread.instance().postGUI(() -> {
 					log.v("synced posting LoadDocument task to GUI thread");
-					post(new LoadDocumentTask(bookInfo, inputStream, doneHandler, errorHandler));
+					post(new LoadDocumentTask(bookInfo, docBuffer, doneHandler, errorHandler));
 				});
 			});
-		});
-		return true;
+			return true;
+		}
+		return false;
 	}
 
 	public boolean loadDocument(String fileName, final Runnable doneHandler, final Runnable errorHandler) {
@@ -3183,17 +3206,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 				errorHandler.run();
 			return false;
 		}
-		BookInfo book = Services.getHistory().getBookInfo(contentPath);
-		if (book != null)
-			log.v("loadDocument() : found book in history : " + book);
-		FileInfo fi = null;
-		if (book == null) {
-			log.v("loadDocument() : book not found in history, building FileInfo by Uri...");
-			fi = new FileInfo(contentPath);
-		} else {
-			fi = book.getFileInfo();
-			log.v("loadDocument() : item from history : " + fi);
-		}
+		FileInfo fi = new FileInfo(contentPath);
 		return loadDocumentFromStream(inputStream, fi, doneHandler, errorHandler);
 	}
 
@@ -5074,7 +5087,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 	private class LoadDocumentTask extends Task {
 		String filename;
 		String path;
-		InputStream inputStream;
+		byte[] docBuffer;
 		Runnable doneHandler;
 		Runnable errorHandler;
 		String pos;
@@ -5082,12 +5095,12 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 		boolean disableInternalStyles;
 		boolean disableTextAutoformat;
 
-		LoadDocumentTask(BookInfo bookInfo, InputStream inputStream, Runnable doneHandler, Runnable errorHandler) {
+		LoadDocumentTask(BookInfo bookInfo, byte[] docBuffer, Runnable doneHandler, Runnable errorHandler) {
 			BackgroundThread.ensureGUI();
 			mBookInfo = bookInfo;
 			FileInfo fileInfo = bookInfo.getFileInfo();
 			log.v("LoadDocumentTask for " + fileInfo);
-			if (fileInfo.getTitle() == null && inputStream == null) {
+			if (fileInfo.getTitle() == null && docBuffer == null) {
 				// As a book 'should' have a title, no title means we should
 				// retrieve the book metadata from the engine to get the
 				// book language.
@@ -5099,7 +5112,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			log.v("update hyphenation language: " + language + " for " + fileInfo.getTitle());
 			this.filename = fileInfo.getPathName();
 			this.path = fileInfo.arcname != null ? fileInfo.arcname : fileInfo.pathname;
-			this.inputStream = inputStream;
+			this.docBuffer = docBuffer;
 			this.doneHandler = doneHandler;
 			this.errorHandler = errorHandler;
 			//FileInfo fileInfo = new FileInfo(filename);
@@ -5109,12 +5122,15 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			//Properties oldSettings = new Properties(mSettings);
 			// TODO: enable storing of profile per book
 			mActivity.setCurrentProfile(profileNumber);
-			if (mBookInfo != null && mBookInfo.getLastPosition() != null)
-				pos = mBookInfo.getLastPosition().getStartPos();
+			Bookmark lastPos = null;
+			if (mBookInfo != null)
+				lastPos = mBookInfo.getLastPosition();
+			if (lastPos != null)
+				pos = lastPos.getStartPos();
 			log.v("LoadDocumentTask : book info " + mBookInfo);
 			log.v("LoadDocumentTask : last position = " + pos);
-			if (mBookInfo != null && mBookInfo.getLastPosition() != null)
-				setTimeElapsed(mBookInfo.getLastPosition().getTimeElapsed());
+			if (lastPos != null)
+				setTimeElapsed(lastPos.getTimeElapsed());
 			//mBitmap = null;
 			//showProgress(1000, R.string.progress_loading);
 			//draw();
@@ -5147,8 +5163,8 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 				doc.doCommand(ReaderCommand.DCMD_SET_RENDER_BLOCK_RENDERING_FLAGS.nativeId, mBookInfo.getFileInfo().blockRenderingFlags);
 			}
 			boolean success;
-			if (null != inputStream)
-				success = doc.loadDocumentFromStream(inputStream, filename);
+			if (null != docBuffer)
+				success = doc.loadDocumentFromBuffer(docBuffer, filename);
 			else
 				success = doc.loadDocument(filename);
 			if (success) {
@@ -5167,10 +5183,20 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 				preparePageImage(0);
 				log.v("updating loaded book info");
 				updateLoadedBookInfo();
-				log.i("Document " + filename + " is loaded successfully");
-				if (pos != null) {
-					log.i("Restoring position : " + pos);
-					restorePositionBackground(pos);
+				if (null == docBuffer) {
+					// Opened existing file
+					log.i("Document " + filename + " is loaded successfully");
+					if (pos != null) {
+						log.i("Restoring position : " + pos);
+						restorePositionBackground(pos);
+					}
+				} else {
+					// Opened from memory buffer
+					log.i("Stream " + filename + " loaded successfully");
+					// restore the last read position and other tasks are
+					// performed in the done () function, since we must
+					// receive data from the database through callbacks
+					// and cannot control the completion of the operation.
 				}
 				CoolReader.dumpHeapAllocation();
 			} else {
@@ -5184,46 +5210,122 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 			BackgroundThread.ensureGUI();
 			log.d("LoadDocumentTask, GUI thread is finished successfully");
 			if (!Services.isStopped()) {
-				Services.getHistory().updateBookAccess(mBookInfo, getTimeElapsed());
-				final BookInfo finalBookInfo = new BookInfo(mBookInfo);
-				mActivity.waitForCRDBService(() -> mActivity.getDB().saveBookInfo(finalBookInfo));
-				if (coverPageBytes != null && mBookInfo.getFileInfo() != null) {
-					// TODO: fix it
-					/*
-					DocumentFormat format = mBookInfo.getFileInfo().format;
-					if (null != format) {
-						if (format.needCoverPageCaching()) {
-//		        			if (mActivity.getBrowser() != null)
-//		        				mActivity.getBrowser().setCoverpageData(new FileInfo(mBookInfo.getFileInfo()), coverPageBytes);
+				if (null == docBuffer) {
+					// Opened from existing file
+					Services.getHistory().updateBookAccess(mBookInfo, getTimeElapsed());
+					final BookInfo finalBookInfo = new BookInfo(mBookInfo);
+					mActivity.waitForCRDBService(() -> mActivity.getDB().saveBookInfo(finalBookInfo));
+					if (coverPageBytes != null && mBookInfo.getFileInfo() != null) {
+						// TODO: fix it
+						/*
+						DocumentFormat format = mBookInfo.getFileInfo().format;
+						if (null != format) {
+							if (format.needCoverPageCaching()) {
+//			        			if (mActivity.getBrowser() != null)
+//			        				mActivity.getBrowser().setCoverpageData(new FileInfo(mBookInfo.getFileInfo()), coverPageBytes);
+							}
+						}
+						*/
+						if (DeviceInfo.EINK_NOOK)
+							updateNookTouchCoverpage(mBookInfo.getFileInfo().getPathName(), coverPageBytes);
+						//mEngine.setProgressDrawable(coverPageDrawable);
+					}
+					if (DeviceInfo.EINK_SONY) {
+						SonyBookSelector selector = new SonyBookSelector(mActivity);
+						long l = selector.getContentId(path);
+						if (l != 0) {
+							selector.setReadingTime(l);
+							selector.requestBookSelection(l);
 						}
 					}
-					*/
-					if (DeviceInfo.EINK_NOOK)
-						updateNookTouchCoverpage(mBookInfo.getFileInfo().getPathName(), coverPageBytes);
-					//mEngine.setProgressDrawable(coverPageDrawable);
-				}
-				if (DeviceInfo.EINK_SONY) {
-					SonyBookSelector selector = new SonyBookSelector(mActivity);
-					long l = selector.getContentId(path);
-					if (l != 0) {
-						selector.setReadingTime(l);
-						selector.requestBookSelection(l);
+					mActivity.setLastBook(filename);
+				} else {
+					// Opened from memory buffer
+					// After stream successfully opened, find corresponding file it in DB
+					// Now mBookInfo already contains updated data
+					if (0 != mBookInfo.getFileInfo().crc32) {
+						ArrayList<String> fingerprints = new ArrayList<String>(1);
+						String fingerprint = Long.toString(mBookInfo.getFileInfo().crc32);
+						fingerprints.add(fingerprint);
+						mActivity.waitForCRDBService(() -> mActivity.getDB().findByFingerprints(10, fingerprints, fileList -> {
+							FileInfo result = null;
+							// TODO: select more recent file
+							//  or may be file with maximum read pos
+							for (FileInfo f : fileList) {
+								if (f.exists()) {
+									result = f;
+									break;
+								}
+							}
+							if (null == result) {
+								// Tier 1, not found or not exist: save stream as file in app private directory,
+								// At this point, the inputStream has already been fully read to the end
+								// and cannot be reset to its original position.
+								// So, we create a new input stream from docBuffer.
+								ByteArrayInputStream inputStream = new ByteArrayInputStream(docBuffer);
+								BookInfo bi = Services.getDocumentCache().saveStream(mBookInfo.getFileInfo(), inputStream);
+								if (null != bi) {
+									mBookInfo = new BookInfo(bi);
+									Services.getHistory().updateBookAccess(mBookInfo, getTimeElapsed());
+									final BookInfo finalBookInfo = new BookInfo(mBookInfo);
+									mActivity.waitForCRDBService(() -> mActivity.getDB().saveBookInfo(finalBookInfo));
+									mActivity.setLastBook(finalBookInfo.getFileInfo().getPathName());
+								} else {
+									log.e("Failed to save document memory buffer to file!");
+									// Show error? Or something other action?
+									// We cannot throw an exception here so that the fail() function
+									// is called later, since we are in the done() function, not work().
+									// And we cannot move this block of code to the work() function,
+									// since we use callback functions to get information from the database,
+									// i.e. this block of code is not continuously executing.
+									// Therefore, we leave this exception unhandled.
+									mActivity.showToast(R.string.failed_to_save_memory_stream);
+								}
+							} else {
+								// Tier 2, found: update mBookInfo, fileInfo, filename, pos
+								mActivity.getDB().loadBookInfo(result, bookInfo -> {
+									if (null != bookInfo) {
+										// ok, bookmarks is loaded
+										mBookInfo = new BookInfo(bookInfo);
+										FileInfo fileInfo = mBookInfo.getFileInfo();
+										filename = fileInfo.getPathName();
+										path = fileInfo.arcname != null ? fileInfo.arcname : fileInfo.pathname;
+										if (mBookInfo.getLastPosition() != null)
+											pos = mBookInfo.getLastPosition().getStartPos();
+										if (pos != null) {
+											final String finalPos = pos;
+											BackgroundThread.instance().executeBackground(() -> {
+												log.i("Restoring position : " + finalPos);
+												restorePositionBackground(finalPos);
+											});
+										}
+										Services.getHistory().updateBookAccess(mBookInfo, getTimeElapsed());
+										final BookInfo finalBookInfo = new BookInfo(mBookInfo);
+										mActivity.waitForCRDBService(() -> mActivity.getDB().saveBookInfo(finalBookInfo));
+										mActivity.setLastBook(filename);
+										if (null != doneHandler)
+											doneHandler.run();
+									} else {
+										// Logic error: not found by pathname, but found by fingerprint
+										log.e("Failed to load bookmarks for book with fingerprint: " + fingerprint);
+										if (null != errorHandler)
+											errorHandler.run();
+									}
+								});
+							}
+						}));
+					} else {
+						log.e("Invalid CRC32 (0)");
+						// See comment above...
 					}
 				}
-				mOpened = true;
-
 				highlightBookmarks();
-
 				hideProgress();
 				drawPage();
-				BackgroundThread.instance().postGUI(() -> {
-					mActivity.showReader();
-					if (null != doneHandler)
-						doneHandler.run();
-				});
-				// Save last opened book ONLY if book opened from real file not stream.
-				if (null == inputStream)
-					mActivity.setLastBook(filename);
+				mActivity.showReader();
+				if (null != doneHandler)
+					doneHandler.run();
+				mOpened = true;
 			}
 		}
 

android/jni/cr3engine.cpp
@@ -824,6 +824,32 @@ JNIEXPORT jbyteArray JNICALL Java_org_coolreader_crengine_Engine_scanBookCoverIn
 
 /*
  * Class:     org_coolreader_crengine_Engine
+ * Method:    isArchiveInternal
+ * Signature: (Ljava/lang/String;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_Engine_isArchiveInternal
+  (JNIEnv * _env, jclass, jstring jarcName)
+{
+    jboolean res = JNI_FALSE;
+    CRJNIEnv env(_env);
+    lString32 arcName = env.fromJavaString(jarcName);
+    LVStreamRef stream = LVOpenFileStream(arcName.c_str(), LVOM_READ);
+    if (!stream.isNull()) {
+        LVContainerRef arc = LVOpenArchieve(stream);
+        if (!arc.isNull()) {
+            if (!DetectEpubFormat(stream) &&
+                !DetectFb3Format(stream) &&
+                !DetectDocXFormat(stream) &&
+                !DetectOpenDocumentFormat(stream)) {
+                res = JNI_TRUE;
+            }
+        }
+    }
+    return res;
+}
+
+/*
+ * Class:     org_coolreader_crengine_Engine
  * Method:    getArchiveItemsInternal
  * Signature: (Ljava/lang/String;)[Ljava/lang/String;
  */
@@ -1179,6 +1205,7 @@ static JNINativeMethod sEngineMethods[] = {
   {"setCacheDirectoryInternal", "(Ljava/lang/String;I)Z", (void*)Java_org_coolreader_crengine_Engine_setCacheDirectoryInternal},
   {"scanBookPropertiesInternal", "(Lorg/coolreader/crengine/FileInfo;)Z", (void*)Java_org_coolreader_crengine_Engine_scanBookPropertiesInternal},
   {"updateFileCRC32Internal", "(Lorg/coolreader/crengine/FileInfo;)Z", (void*)Java_org_coolreader_crengine_Engine_updateFileCRC32Internal},
+  {"isArchiveInternal", "(Ljava/lang/String;)Z", (void*)Java_org_coolreader_crengine_Engine_isArchiveInternal},
   {"getArchiveItemsInternal", "(Ljava/lang/String;)[Ljava/lang/String;", (void*)Java_org_coolreader_crengine_Engine_getArchiveItemsInternal},
   {"isLink", "(Ljava/lang/String;)Ljava/lang/String;", (void*)Java_org_coolreader_crengine_Engine_isLink},
   {"suspendLongOperationInternal", "()V", (void*)Java_org_coolreader_crengine_Engine_suspendLongOperationInternal},
@@ -1208,7 +1235,7 @@ static JNINativeMethod sDocViewMethods[] = {
   {"getCurrentPageBookmarkInternal", "()Lorg/coolreader/crengine/Bookmark;", (void*)Java_org_coolreader_crengine_DocView_getCurrentPageBookmarkInternal},
   {"goToPositionInternal", "(Ljava/lang/String;Z)Z", (void*)Java_org_coolreader_crengine_DocView_goToPositionInternal},
   {"getPositionPropsInternal", "(Ljava/lang/String;Z)Lorg/coolreader/crengine/PositionProperties;", (void*)Java_org_coolreader_crengine_DocView_getPositionPropsInternal},
-  {"updateBookInfoInternal", "(Lorg/coolreader/crengine/BookInfo;)V", (void*)Java_org_coolreader_crengine_DocView_updateBookInfoInternal},
+  {"updateBookInfoInternal", "(Lorg/coolreader/crengine/BookInfo;Z)V", (void*)Java_org_coolreader_crengine_DocView_updateBookInfoInternal},
   {"getTOCInternal", "()Lorg/coolreader/crengine/TOCItem;", (void*)Java_org_coolreader_crengine_DocView_getTOCInternal},
   {"clearSelectionInternal", "()V", (void*)Java_org_coolreader_crengine_DocView_clearSelectionInternal},
   {"findTextInternal", "(Ljava/lang/String;III)Z", (void*)Java_org_coolreader_crengine_DocView_findTextInternal},

android/jni/cr3java.h
@@ -186,6 +186,26 @@ public:
 	void set(lInt64 v) { objacc->SetLongField(objacc.getObject(), fieldid, v); } 
 };
 
+class CRBooleanField : public CRFieldAccessor {
+public:
+	CRBooleanField( CRObjectAccessor & acc, const char * fieldName )
+			: CRFieldAccessor( acc, fieldName, "Z" )
+	{
+	}
+	bool get() { return objacc->GetBooleanField(objacc.getObject(), fieldid); }
+	void set(bool v) { objacc->SetBooleanField(objacc.getObject(), fieldid, v); }
+};
+
+class CRObjectField : public CRFieldAccessor {
+public:
+	CRObjectField( CRObjectAccessor & acc, const char * fieldName, const char* signature )
+			: CRFieldAccessor( acc, fieldName, signature )
+	{
+	}
+	jobject get() { return objacc->GetObjectField(objacc.getObject(), fieldid); }
+	void set(jobject v) { objacc->SetObjectField(objacc.getObject(), fieldid, v); }
+};
+
 class BitmapAccessor : public CRJNIEnv {
 private:
     jobject bitmap;

android/jni/docview.h
@@ -28,9 +28,9 @@ public:
 	bool closeBook();
 	bool loadHistory( lString32 filename );
 	bool saveHistory( lString32 filename );
-	void createDefaultDocument( lString32 title, lString32 message );
-	bool loadDocument( lString32 filename );
-	bool loadDocument( LVStreamRef stream, lString32 contentPath );
+	void createDefaultDocument( const lString32& title, const lString32& message );
+	bool loadDocument( const lString32& filename );
+	bool loadDocument( LVStreamRef stream, const lString32& contentPath );
 	int doCommand( int cmd, int param );
     bool findText( lString32 pattern, int origin, bool reverse, bool caseInsensitive );
     void clearSelection();

android/jni/org_coolreader_crengine_DocView.h
@@ -136,10 +136,10 @@ JNIEXPORT jobject JNICALL Java_org_coolreader_crengine_DocView_getPositionPropsI
 /*
  * Class:     org_coolreader_crengine_DocView
  * Method:    updateBookInfoInternal
- * Signature: (Lorg/coolreader/crengine/BookInfo;)V
+ * Signature: (Lorg/coolreader/crengine/BookInfo;Z)V
  */
 JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_updateBookInfoInternal
-  (JNIEnv *, jobject, jobject);
+  (JNIEnv *, jobject, jobject, jboolean);
 
 /*
  * Class:     org_coolreader_crengine_DocView

android/jni/org_coolreader_crengine_Engine.h
@@ -85,6 +85,14 @@ JNIEXPORT jintArray JNICALL Java_org_coolreader_crengine_Engine_getAvailableSynt
 
 /*
  * Class:     org_coolreader_crengine_Engine
+ * Method:    isArchiveInternal
+ * Signature: (Ljava/lang/String;)Z
+ */
+JNIEXPORT jboolean JNICALL Java_org_coolreader_crengine_Engine_isArchiveInternal
+  (JNIEnv *, jclass, jstring);
+
+/*
+ * Class:     org_coolreader_crengine_Engine
  * Method:    getArchiveItemsInternal
  * Signature: (Ljava/lang/String;)[Ljava/lang/String;
  */

android/res/values-ru/strings.xml
@@ -735,4 +735,5 @@
     <string name="filter_by_book_language_s">Отфильтровать по языку книги (%s)</string>
     <string name="scanning_font_files">Сканирование файлов шрифтов…</string>
     <string name="undetermined">Неопределенный</string>
+    <string name="failed_to_save_memory_stream">Не удалось сохранить внешний поток в хранилище устройства!</string>
 </resources>

commit bd998ae661ab74e96a74c002e58959ebfe896976

Fixed BUG introduced in 4b98d13dc3b828f3749bfe3c9a459610c91b3ab3 (#308): incorrect path in updated BookInfo object after book is loaded.

android/jni/docview.cpp
@@ -1851,27 +1851,32 @@ JNIEXPORT void JNICALL Java_org_coolreader_crengine_DocView_updateBookInfoIntern
     if (_updatePath) {
 		CRPropRef doc_props = p->_docview->getDocProps();
 		if (!doc_props.isNull()) {
+			CRStringField arcnameField(fileinfo, "arcname");
+			CRLongField arcsizeField(fileinfo, "arcsize");
+			CRBooleanField isArchiveField(fileinfo, "isArchive");
+			CRStringField pathnameField(fileinfo, "pathname");
+			CRStringField pathField(fileinfo, "path");
+			CRStringField filenameField(fileinfo, "filename");
+			CRLongField sizeField(fileinfo, "size");
 			bool isArchive = !doc_props->getStringDef(DOC_PROP_ARC_NAME, "").empty();
 			if (isArchive) {
 				lString32 arcname = doc_props->getStringDef(DOC_PROP_ARC_NAME, "");
+				lString32 arcpath = doc_props->getStringDef(DOC_PROP_ARC_PATH, "");
 				int arcsize = doc_props->getIntDef(DOC_PROP_ARC_SIZE, 0);
-				CRStringField arcnameField(fileinfo, "arcname");
-				arcnameField.set(arcname);
-				CRLongField arcsizeField(fileinfo, "arcsize");
+				arcnameField.set(LVCombinePaths(arcpath, arcname));
 				arcsizeField.set(arcsize);
+			} else {
+				arcnameField.setObject(NULL);
+				arcsizeField.set(0);
 			}
-			lString32 pathname = doc_props->getStringDef(DOC_PROP_FILE_NAME, "");
-			lString32 filename = LVExtractFilename(pathname);
+			lString32 path = doc_props->getStringDef(DOC_PROP_FILE_PATH, "");
+			LVRemovePathDelimiter(path);
+			lString32 filename = doc_props->getStringDef(DOC_PROP_FILE_NAME, "");
 			int size = doc_props->getIntDef(DOC_PROP_FILE_SIZE, 0);
-			CRBooleanField isArchiveField(fileinfo, "isArchive");
 			isArchiveField.set(isArchive);
-			CRStringField pathnameField(fileinfo, "pathname");
-			CRStringField pathField(fileinfo, "path");
-			pathnameField.set(pathname);
-			pathField.set(pathname);
-			CRStringField filenameField(fileinfo, "filename");
+			pathnameField.set(LVCombinePaths(path, filename));
+			pathField.set(path);
 			filenameField.set(filename);
-			CRLongField sizeField(fileinfo, "size");
 			sizeField.set(size);
 		}
 	}

android/src/org/coolreader/crengine/ReaderView.java
@@ -2658,10 +2658,10 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 
 	//private File historyFile;
 
-	private void updateLoadedBookInfo() {
+	private void updateLoadedBookInfo(boolean updatePath) {
 		BackgroundThread.ensureBackground();
 		// get title, authors, genres, etc.
-		doc.updateBookInfo(mBookInfo, true);
+		doc.updateBookInfo(mBookInfo, updatePath);
 		updateCurrentPositionStatus();
 		// check whether current book properties updated on another devices
 		// TODO: fix and reenable
@@ -5182,7 +5182,7 @@ public class ReaderView implements android.view.SurfaceHolder.Callback, Settings
 				}
 				preparePageImage(0);
 				log.v("updating loaded book info");
-				updateLoadedBookInfo();
+				updateLoadedBookInfo(null != docBuffer);
 				if (null == docBuffer) {
 					// Opened existing file
 					log.i("Document " + filename + " is loaded successfully");