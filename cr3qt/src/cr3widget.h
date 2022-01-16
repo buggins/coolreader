7f6844ce5597fb7c7e1d967713bdd7e391037c12
Aleksey Chernov <valexlin@gmail.com>
2022-01-16 20:38:35 +0300

    Improve HiDPI support for Qt5

--- cr3qt/src/cr3widget.h
+++ cr3qt/src/cr3widget.h
@@ -173,6 +173,7 @@ class CR3View : public QWidget, public LVDocViewCallback
         DocViewData * _data; // to hide non-qt implementation
         LVDocView * _docview;
         QScrollBar * _scroll;
+        qreal _dpr;  // screen display pixel ratio (for HiDPI screens)
         PropsChangeCallback * _propsCallback;
         QStringList _hyphDicts;
         QCursor _normalCursor;
