11ec8efda3f9af09559069caefecba8e759ae274
Aleksey Chernov <valexlin@gmail.com>
2022-06-06 10:57:31 +0400

    thirdpart zlib: bump to 1.2.12.

--- thirdparty_repo/zlib.meta.sh
+++ thirdparty_repo/zlib.meta.sh
@@ -2,12 +2,12 @@
 # Metadata for deploy script
 
 PN="zlib"
-PV="1.2.11"
+PV="1.2.12"
 # package revision: when patchset is changed (but not version), increase it
 # when version changed, reset to "1".
 REV="1"
 SRCFILE="${PN}-${PV}.tar.xz"
-SHA512="b7f50ada138c7f93eb7eb1631efccd1d9f03a5e77b6c13c8b757017b2d462e19d2d3e01c50fad60a4ae1bc86d431f6f94c72c11ff410c25121e571953017cb67"
+SHA512="12940e81e988f7661da52fa20bdc333314ae86a621fdb748804a20840b065a1d6d984430f2d41f3a057de0effc6ff9bcf42f9ee9510b88219085f59cbbd082bd"
 
 URL="http://www.zlib.net/${SRCFILE}"
 
