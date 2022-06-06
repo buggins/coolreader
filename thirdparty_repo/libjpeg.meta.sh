e90eb966102bb315b8232df00a7bfdf37a96b920
Aleksey Chernov <valexlin@gmail.com>
2022-06-06 10:53:34 +0400

    thirdparty libjpeg: for any reason remote file jpegsrc.v9d.tar.gz is changed but it's contents is NOT. So it is safely to update SHA512 sum for this file.

--- thirdparty_repo/libjpeg.meta.sh
+++ thirdparty_repo/libjpeg.meta.sh
@@ -7,7 +7,7 @@ PV="9.4.0"
 # when version changed, reset to "1".
 REV="1"
 SRCFILE="jpegsrc.v9d.tar.gz"
-SHA512="6515a6f617fc9da7a3d7b4aecc7d78c4ee76159d36309050b7bf9f9672b4e29c2f34b1f4c3d7d65d7f6e2c104c49f53dd2e3b45eac22b1576d2cc54503faf333"
+SHA512="c64d3ee269367351211c077a64b2395f2cfa49b9f8257fae62fa1851dc77933a44b436d8c70ceb52b73a5bedff6dbe560cc5d6e3ed5f2997d724e2ede9582bc3"
 
 URL="http://www.ijg.org/files/${SRCFILE}"
 
