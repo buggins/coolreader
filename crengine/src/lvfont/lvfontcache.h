49ff52be51933705dceee40dfc128b9c71becd81
Aleksey Chernov <valexlin@gmail.com>
2022-10-28 23:28:46 +0400

    Update source file headers in crengine/src/lvfont/: use the standard GPLv2+ header. File authors reconstructed.

--- crengine/src/lvfont/lvfontcache.h
+++ crengine/src/lvfont/lvfontcache.h
@@ -1,16 +1,30 @@
-/** \file lvfontcache.h
-    \brief font cache
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2007,2008,2011,2012 Vadim Lopatin <coolreader.org@gmail.com>
+ *   Copyright (C) 2015 Yifei(Frank) ZHU <fredyifei@gmail.com>             *
+ *   Copyright (C) 2017,2018 poire-z <poire-z@users.noreply.github.com>    *
+ *   Copyright (C) 2018,2020,2021 Aleksey Chernov <valexlin@gmail.com>     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
-    CoolReader Engine
-
-
-    (c) Vadim Lopatin, 2000-2006
-    This source code is distributed under the terms of
-    GNU General Public License.
-
-    See LICENSE file for details.
-
-*/
+/**
+ * \file lvfontcache.h
+ * \brief font cache
+ */
 
 #ifndef __LV_FONTCACHE_H_INCLUDED__
 #define __LV_FONTCACHE_H_INCLUDED__
--- crengine/src/lvfont/lvwin32fontman.h
+++ crengine/src/lvfont/lvwin32fontman.h
@@ -1,16 +1,29 @@
-/** \file lvwin32fontman.cpp
-    \brief Win32 font manager interface
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2007 Vadim Lopatin <coolreader.org@gmail.com>           *
+ *   Copyright (C) 2020,2021 Aleksey Chernov <valexlin@gmail.com>          *
+ *   Copyright (C) 2020 poire-z <poire-z@users.noreply.github.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
-    CoolReader Engine
-
-
-    (c) Vadim Lopatin, 2000-2006
-    This source code is distributed under the terms of
-    GNU General Public License.
-
-    See LICENSE file for details.
-
-*/
+/**
+ * \file lvwin32fontman.h
+ * \brief Win32 font manager interface
+ */
 
 #ifndef __LV_WIN32FONTMAN_H_INCLUDED__
 #define __LV_WIN32FONTMAN_H_INCLUDED__
