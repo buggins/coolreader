9d551d1ee53e03ce4f6c2672deef0b46f530e2d7
Aleksey Chernov <valexlin@gmail.com>
2022-10-31 02:11:14 +0400

    Update source file headers in crengine/src/lvimg/: use the standard GPLv2+ header. File authors reconstructed.

--- crengine/src/lvimg/lvjpegimagesource.h
+++ crengine/src/lvimg/lvjpegimagesource.h
@@ -1,15 +1,22 @@
-/** @file lvjpegimagesource.h
-    @brief library private stuff: jpeg image decoder
-
-    CoolReader Engine
-
-    (c) Vadim Lopatin, 2000-2006
-    This source code is distributed under the terms of
-    GNU General Public License.
-
-    See LICENSE file for details.
-
-*/
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2007,2012 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #ifndef __LVJPEGIMAGESOURCE_H_INCLUDED__
 #define __LVJPEGIMAGESOURCE_H_INCLUDED__
--- crengine/src/lvimg/lvnodeimagesource.h
+++ crengine/src/lvimg/lvnodeimagesource.h
@@ -1,15 +1,23 @@
-/** @file lvnodeimagesource.h
-    @brief library private stuff
-
-    CoolReader Engine
-
-    (c) Vadim Lopatin, 2000-2006
-    This source code is distributed under the terms of
-    GNU General Public License.
-
-    See LICENSE file for details.
-
-*/
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2007 Vadim Lopatin <coolreader.org@gmail.com>           *
+ *   Copyright (C) 2021 NiLuJe <ninuje@gmail.com>                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #ifndef __LVNODEIMAGESOURCE_H_INCLUDED__
 #define __LVNODEIMAGESOURCE_H_INCLUDED__
