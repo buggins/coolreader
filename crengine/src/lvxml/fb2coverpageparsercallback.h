19c41ee9e4e5801b4d773ba4cfc171b14f553db9
Aleksey Chernov <valexlin@gmail.com>
2022-11-02 18:35:19 +0400

    Update source file headers in crengine/src/lvxml/: use the standard GPLv2+ header. File authors reconstructed.

--- crengine/src/lvxml/fb2coverpageparsercallback.h
+++ crengine/src/lvxml/fb2coverpageparsercallback.h
@@ -1,16 +1,24 @@
-/** @file fb2coverpageparsercallback.h
-    @brief library private stuff
-
-   CoolReader Engine
-
-   (c) Vadim Lopatin, 2000-2006
-
-   This source code is distributed under the terms of
-   GNU General Public License.
-
-   See LICENSE file for details.
-
-*/
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2012 Vadim Lopatin <coolreader.org@gmail.com>           *
+ *   Copyright (C) 2020 poire-z <poire-z@users.noreply.github.com>         *
+ *   Copyright (C) 2020 Aleksey Chernov <valexlin@gmail.com>               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #ifndef __FB2COVERPAGEPARSERCALLBACK_H_INCLUDED__
 #define __FB2COVERPAGEPARSERCALLBACK_H_INCLUDED__
