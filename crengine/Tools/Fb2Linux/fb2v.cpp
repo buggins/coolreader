78028e6dbb564bd0200fd4c96a2b66d9f6972cd6
Aleksey Chernov <valexlin@gmail.com>
2022-11-02 22:22:11 +0400

    Update source file header in crengine/Tools/*/: use the standard GPLv2+ header. File authors reconstructed.

--- crengine/Tools/Fb2Linux/fb2v.cpp
+++ crengine/Tools/Fb2Linux/fb2v.cpp
@@ -1,4 +1,25 @@
-// Written by Ch. Tronche (http://tronche.lri.fr:8000/)
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2007,2008 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *   Copyright (C) 2019,2020 Aleksey Chernov <valexlin@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
+// Based on Ch. Tronche work (http://tronche.lri.fr:8000/)
 // Copyright by the author. This is unmaintained, no-warranty free software. 
 // Please use freely. It is appreciated (but by no means mandatory) to
 // acknowledge the author's contribution. Thank you.
--- crengine/Tools/glyphcache_bench/lvfontglyphcache_b.cpp
+++ crengine/Tools/glyphcache_bench/lvfontglyphcache_b.cpp
@@ -1,16 +1,23 @@
-/** @file lvfntman.cpp
-    @brief font glyph cache implementation
-
-    CoolReader Engine
-
-
-    (c) Vadim Lopatin, 2000-2006
-    This source code is distributed under the terms of
-    GNU General Public License.
-
-    See LICENSE file for details.
-
-*/
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2011,2012,2014 Vadim Lopatin <coolreader.org@gmail.com> *
+ *   Copyright (C) 2019,2020 Aleksey Chernov <valexlin@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #include "lvfontglyphcache_b.h"
 #include "../../include/crlocks.h"
