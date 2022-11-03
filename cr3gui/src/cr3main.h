a7361dd8d8455b0a063c7e90eafca088d851759c
Aleksey Chernov <valexlin@gmail.com>
2022-11-03 18:34:10 +0400

    Update source file header in cr3gui/: use the standard GPLv2+ header. File authors reconstructed.

--- cr3gui/src/cr3main.h
+++ cr3gui/src/cr3main.h
@@ -1,14 +1,28 @@
+/***************************************************************************
+ *   CoolReader GUI                                                        *
+ *   Copyright (C) 2008,2009 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *   Copyright (C) 2008 Alexander V. Nikolaev <avn@daemon.hole.ru>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
 //
 // C++ Interface: settings
 //
-// Description: 
-//
-//
-// Author: Vadim Lopatin <vadim.lopatin@coolreader.org>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+
 #ifndef CR3MAIN_H_INCLUDED
 #define CR3MAIN_H_INCLUDED
 
--- cr3gui/src/selnavig.cpp
+++ cr3gui/src/selnavig.cpp
@@ -1,14 +1,27 @@
+/***************************************************************************
+ *   CoolReader GUI                                                        *
+ *   Copyright (C) 2009-2011 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *   Copyright (C) 2011 Konstantin Potapov <pkbo@users.sourceforge.net>    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
 //
 // C++ Implementation: selection navigation dialog
 //
-// Description:
-//      Shows keyboard, and allows to input text string
-//
-// Author: Vadim Lopatin <vadim.lopatin@coolreader.org>, (C) 2009
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-// selnavig.cpp
 
 #include "selnavig.h"
 
