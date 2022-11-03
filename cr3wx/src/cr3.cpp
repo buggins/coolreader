ea41f182a60939b60959f60138071f0c2bc6a776
Aleksey Chernov <valexlin@gmail.com>
2022-11-03 10:33:44 +0400

    Update source file header in cr3wx/: use the standard GPLv2+ header. File authors reconstructed.

--- cr3wx/src/cr3.cpp
+++ cr3wx/src/cr3.cpp
@@ -1,3 +1,26 @@
+/***************************************************************************
+ *   CoolReader, wxWidgets GUI                                             *
+ *   Copyright (C) 2007-2010,2012 Vadim Lopatin <coolreader.org@gmail.com> *
+ *   Copyright (C) 2008 Torque <torque@mail.ru>                            *
+ *   Copyright (C) 2018 Sergey Torokhov <torokhov-s-a@yandex.ru>           *
+ *   Copyright (C) 2020,2021 Aleksey Chernov <valexlin@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
 #include <wx/wx.h>
 #include <wx/mstream.h>
 #include <wx/stdpaths.h>
--- cr3wx/src/view.h
+++ cr3wx/src/view.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *   CoolReader, wxWidgets GUI                                             *
+ *   Copyright (C) 2007-2010 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *   Copyright (C) 2008 Torque <torque@mail.ru>                            *
+ *   Copyright (C) 2020 Aleksey Chernov <valexlin@gmail.com>               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #ifndef _CR3VIEW_H_
 #define _CR3VIEW_H_
@@ -5,8 +26,6 @@
 #include <crgui.h>
 /**
  * @short XML Document View window
- * @author Vadim Lopatin <vadim.lopatin@coolreader.org>
- * @version 0.1
  */
 
 class
