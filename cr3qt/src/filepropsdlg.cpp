4f4ca03a18b42d4e046582986d40e9a83e7ca3d3
Aleksey Chernov <valexlin@gmail.com>
2022-11-03 09:23:09 +0400

    Update source file header in cr3qt/: use the standard GPLv2+ header. File authors reconstructed.

--- cr3qt/src/filepropsdlg.cpp
+++ cr3qt/src/filepropsdlg.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *   CoolReader, Qt GUI                                                    *
+ *   Copyright (C) 2009,2010,2012,2014 Vadim Lopatin <coolreader.org@gmail.com>
+ *   Copyright (C) 2020,2021 Aleksey Chernov <valexlin@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
 #include "filepropsdlg.h"
 #include "ui_filepropsdlg.h"
 #include "cr3widget.h"
