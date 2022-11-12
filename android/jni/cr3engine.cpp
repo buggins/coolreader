c3799cb1f1cb6ec7df1a25a768e215e6760e2e15
Aleksey Chernov <valexlin@gmail.com>
2022-11-12 12:54:01 +0400

    Update source file header in android/jni: use the standard GPLv2+ header. File authors reconstructed.

--- android/jni/cr3engine.cpp
+++ android/jni/cr3engine.cpp
@@ -1,3 +1,25 @@
+/***************************************************************************
+ *   CoolReader engine                                                     *
+ *   Copyright (C) 2010-2015,2020 Vadim Lopatin <coolreader.org@gmail.com> *
+ *   Copyright (C) 2012 Daniel Savard <daniels@xsoli.com>                  *
+ *   Copyright (C) 2018-2021 Aleksey Chernov <valexlin@gmail.com>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
+
 // CoolReader3 Engine JNI interface
 // BASED on Android NDK Plasma example
 
