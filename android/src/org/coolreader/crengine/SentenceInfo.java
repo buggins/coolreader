740d6129264d405e5157f003d7b0b0ee849036c4
Elliot Wolk <elliot.wolk@gmail.com>
2023-04-07 19:24:18 -0400

    jni[docview]: getAllSentences() to get coords and text of all sentences

---
+++ android/src/org/coolreader/crengine/SentenceInfo.java
@@ -0,0 +1,20 @@
+package org.coolreader.crengine;
+
+public class SentenceInfo {
+	public int startX;
+	public int startY;
+	public String text;
+
+	public SentenceInfo() {
+	}
+
+	public void setStartX(int startX){
+		this.startX = startX;
+	}
+	public void setStartY(int startY){
+		this.startY = startY;
+	}
+	public void setText(String text){
+		this.text = text;
+	}
+}
